7cf3b7d8e6df2bf9ae3438e79f8b6538140fdd1a
camizanette <camizanette@gmail.com>
2016-07-05 23:26:18 -0700

    Fixing review comments

--- build/lib/smarty/tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-from functools import partial
-from smarty.utils import get_data_filename
-from unittest import TestCase
-
-import smarty
-
-class TestUtils(TestCase):
-    def test_read_molecules(self):
-        molecules = smarty.utils.read_molecules(get_data_filename('molecules/zinc-subset-tripos.mol2.gz'), verbose=False)
