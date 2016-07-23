6c0707eb7f43ff72e4a02099f0659156721f86d7
davidlmobley <dmobley@gmail.com>
2016-07-23 13:07:42 -0700

    Adding smaller parm@frosst subset and script for creation

---
+++ examples/parm@frosst/make_subset.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+
+"""Take the ZINC subset here and make a smaller subset of it for testing purposes."""
+
+from openeye.oechem import *
+
+nmols = 500 #Number of molecules to retain out of full ~7500
+# Currently the above are taken as the first 500. We could also take randomly.
+
+
+
+# Read set with tripos types, write subset
+ifs = oemolistream( 'molecules/zinc-subset-tripos.mol2.gz')
+ofs = oemolostream( 'molecules/zinc-subset-%s-tripos.mol2.gz' % nmols )
+mol = OEMol()
+ct=0
+while OEReadMolecule(ifs, mol) and ct < nmols:
+    OEWriteConstMolecule(ofs, mol)
+    ct += 1
+
+
+# Read set with parm@frosst types, write subset
+# Use flavors here to ensure writing doesn't mangle atom types
+ifs = oemolistream( 'molecules/zinc-subset-parm@frosst.mol2.gz')
+flavor = OEIFlavor_Generic_Default | OEIFlavor_MOL2_Default | OEIFlavor_MOL2_Forcefield
+ifs.SetFlavor(OEFormat_MOL2, flavor)
+ofs = oemolostream( 'molecules/zinc-subset-%s-parm@frosst.mol2.gz' % nmols )
+mol = OEMol()
+ct=0
+while OEReadMolecule(ifs, mol) and ct < nmols:
+    OEWriteConstMolecule(ofs, mol)
+    ct+=1
+
