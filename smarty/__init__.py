aa6a4a5ab0d41fba15a179fbc0b1b6d80b91baee
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-08-07 17:59:54 -0400

    Don't die if openeye tools can't be imported

--- smarty/__init__.py
+++ smarty/__init__.py
@@ -1,7 +1,16 @@
-from .atomtyper import *
-from .forcefield import *
-from .forcefield_utils import *
-from .forcefield_labeler import *
-from .sampler import *
-from .utils import *
+try:
+    import openeye
+    # These can only be imported if openeye tools are available
+    from .atomtyper import *
+    from .forcefield import *
+    from .forcefield_utils import *
+    from .forcefield_labeler import *
+    from .sampler import *
+    from .utils import *
+
+except Exception as e:
+    print(e)
+    print('Warning: Cannot import openeye toolkit; not all functionality will be available.')
+
+from .environment import *
 from .score_utils import *
