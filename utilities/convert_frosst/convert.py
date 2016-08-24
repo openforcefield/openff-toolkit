a7811aa4162dba7918328799c4bef67606d51f24
davidlmobley <dmobley@gmail.com>
2016-08-23 21:30:08 -0700

    Switching convert.py to function with optional CLI.

--- utilities/convert_frosst/convert.py
+++ utilities/convert_frosst/convert.py
@@ -4,13 +4,8 @@ import lxml.etree as etree
 import numpy as np
 from smarty import ForceField
 
-# Define what file we will parse and where we will write to
-infile = 'example.frcmod'
-inxml = 'template.ffxml'
-outxml = 'example.ffxml'
-
 # Function definitions for parsing sections within parameter file
-def parse_nonbon_line( line ):
+def _parse_nonbon_line( line ):
     """Parse an AMBER frcmod nonbon line and return relevant parameters in a dictionary. AMBER uses rmin_half and epsilon in angstroms and kilocalories per mole."""
     tmp = line.split()
     params = {}
@@ -20,7 +15,7 @@ def parse_nonbon_line( line ):
 
     return params
 
-def parse_bond_line( line ):
+def _parse_bond_line( line ):
     """Parse an AMBER frcmod BOND line and return relevant parameters in a dictionary. AMBER uses length and force constant, with the factor of two dropped. Here we multiply by the factor of two before returning. Units are angstroms and kilocalories per mole per square angstrom."""
 
     tmp = line.split()
@@ -30,7 +25,7 @@ def parse_bond_line( line ):
     params['length'] = tmp[2]
     return params
 
-def parse_angl_line( line ):
+def _parse_angl_line( line ):
     """Parse an AMBER frcmod ANGL line and return relevant parameters in a dictionary. AMBER uses angle and force constant, with the factor of two dropped. Here we multiply by the factor of two before returning. Units are degrees and kilocalories per mole."""
 
     tmp = line.split()
@@ -40,7 +35,7 @@ def parse_angl_line( line ):
     params['angle'] = tmp[2]
     return params
 
-def parse_dihe_line( line ):
+def _parse_dihe_line( line ):
     """Parse an AMBER frcmod DIHE line and return relevant parameters in a dictionary. Units for k are kilocalories per mole."""
 
     tmp = line.split()
@@ -52,8 +47,7 @@ def parse_dihe_line( line ):
     params['periodicity1'] = str(int(np.abs(float(tmp[4]))))
     return params
 
-
-def parse_impr_line( line ):
+def _parse_impr_line( line ):
     """Parse an AMBER frcmod DIHE line and return relevant parameters in a dictionary. Units for k are kilocalories per mole."""
 
     tmp = line.split()
@@ -64,103 +58,144 @@ def parse_impr_line( line ):
     params['periodicity1'] = str(int(np.abs(float(tmp[3]))))
     return params
 
-# Obtain sections from target file
-file = open(infile, 'r')
-text = file.readlines()
-file.close()
-sections = {}
-# Section names from frcmod which we will parse
-secnames = ['NONBON', 'BOND', 'ANGL', 'IMPR', 'DIHE']
-# Tags that will be used in the FFXML for these (same order)
-tag = ['Atom', 'Bond', 'Angle', 'Improper', 'Proper']
-# Force names in the FFXML (same order)
-force_section = ['NonbondedForce', 'HarmonicBondForce', 'HarmonicAngleForce', 'PeriodicTorsionForce', 'PeriodicTorsionForce']
-ct = 0
-thissec = None
-while ct < len(text):
-    line = text[ct]
-    tmp = line.split()
+# Main conversion functionality
+def convert_frcmod_to_ffxml( infile, inxml, outxml ):
+    """Convert a modified AMBER frcmod (with SMIRKS replacing atom types) to SMIRFF ffxml format by inserting parameters into a template ffxml file.
 
-    # Skip lines starting with comment or which are blank
-    if line[0]=='#' or len(tmp) < 1:
-        ct+=1
-        continue
+    Parameters
+    ----------
+    infile : str
+        File name of input SMIRKS-ified frcmod file containing parameters
+    inxml : str
+        File name of template SMIRFF FFXML file into which to insert these parameters.
+    outxml : str
+        File name of resulting output SMIRFF FFXML
 
-    # Check first entry to see if it's a section name, if so initialize storage
-    if tmp[0] in secnames:
-        thissec = tmp[0]
-        sections[thissec] = []
-    # Otherwise store
-    else:
-        sections[thissec].append(line)
-
-    ct+=1
+    Notes:
+    -------
+    Input XML file will normally be the template of a SMIRFF XML file without any parameters present (but with requisite force types already specified).
+    """
 
 
-# Read template forcefield file
-ff = ForceField(inxml)
-# Use functions to parse sections from target file and add parameters to force field
-param_id_by_section={}
-param_prefix_by_sec = {'NONBON':'n' , 'BOND':'b', 'ANGL':'a', 'DIHE':'t', 'IMPR':'i'}
-for (idx, name) in enumerate(secnames):
-    param_id_by_section[name] = 1
-    for line in sections[name]:
-        # Parse line for parameters
-        if name=='NONBON':
-            params = parse_nonbon_line(line)
-        elif name=='BOND':
-            params = parse_bond_line(line)
-        elif name=='DIHE':
-            params = parse_dihe_line(line)
-        elif name=='IMPR':
-            params = parse_impr_line(line)
-        elif name=='ANGL':
-            params = parse_angl_line(line)
+    # Obtain sections from target file
+    file = open(infile, 'r')
+    text = file.readlines()
+    file.close()
+    sections = {}
+    # Section names from frcmod which we will parse
+    secnames = ['NONBON', 'BOND', 'ANGL', 'IMPR', 'DIHE']
+    # Tags that will be used in the FFXML for these (same order)
+    tag = ['Atom', 'Bond', 'Angle', 'Improper', 'Proper']
+    # Force names in the FFXML (same order)
+    force_section = ['NonbondedForce', 'HarmonicBondForce', 'HarmonicAngleForce', 'PeriodicTorsionForce', 'PeriodicTorsionForce']
+    ct = 0
+    thissec = None
+    while ct < len(text):
+        line = text[ct]
+        tmp = line.split()
 
-        # Add parameter ID
-        params['id'] = param_prefix_by_sec[name]+str( param_id_by_section[name] )
+        # Skip lines starting with comment or which are blank
+        if line[0]=='#' or len(tmp) < 1:
+            ct+=1
+            continue
 
-        smirks = params['smirks']
-
-        # If it's not a torsion, just store in straightforward way
-        if not (name=='IMPR' or name=='DIHE'):
-            # Check for duplicates first
-            if ff.getParameter( smirks, force_type = force_section[idx] ):
-                raise ValueError("Error: parameter for %s is already present in forcefield." % smirks )
-            else:
-                ff.addParameter( params, smirks, force_section[idx], tag[idx] )
-
-            # Increment parameter id
-            param_id_by_section[name] +=1
-        # If it's a torsion, check to see if there are already parameters and
-        # if so, add a new term to this torsion
+        # Check first entry to see if it's a section name, if so initialize storage
+        if tmp[0] in secnames:
+            thissec = tmp[0]
+            sections[thissec] = []
+        # Otherwise store
         else:
-            # If we have parameters already
-            oldparams = ff.getParameter(smirks, force_type=force_section[idx])
-            if oldparams:
-                # Find what number to use
-                idnr = 1
-                paramtag = 'k%s' % idnr
-                while paramtag in params:
-                    idnr+=1
-                    paramtag = 'k%s' % idnr
-                # Construct new param object with updated numbers
-                for paramtag in ('periodicity1', 'phase1', 'idivf1', 'k1'):
-                    if paramtag in params:
-                        val = params.pop(paramtag)
-                        oldparams[paramtag[:-1]+str(idnr) ] = val
-                # Store
-                ff.setParameter( oldparams, smirks=smirks, force_type=force_section[idx])
+            sections[thissec].append(line)
+
+        ct+=1
+
+
+    # Read template forcefield file
+    ff = ForceField(inxml)
+    # Use functions to parse sections from target file and add parameters to force field
+    param_id_by_section={}
+    param_prefix_by_sec = {'NONBON':'n' , 'BOND':'b', 'ANGL':'a', 'DIHE':'t', 'IMPR':'i'}
+    for (idx, name) in enumerate(secnames):
+        param_id_by_section[name] = 1
+        for line in sections[name]:
+            # Parse line for parameters
+            if name=='NONBON':
+                params = _parse_nonbon_line(line)
+            elif name=='BOND':
+                params = _parse_bond_line(line)
+            elif name=='DIHE':
+                params = _parse_dihe_line(line)
+            elif name=='IMPR':
+                params = _parse_impr_line(line)
+            elif name=='ANGL':
+                params = _parse_angl_line(line)
+
+            # Add parameter ID
+            params['id'] = param_prefix_by_sec[name]+str( param_id_by_section[name] )
+
+            smirks = params['smirks']
+
+            # If it's not a torsion, just store in straightforward way
+            if not (name=='IMPR' or name=='DIHE'):
+                # Check for duplicates first
+                if ff.getParameter( smirks, force_type = force_section[idx] ):
+                    raise ValueError("Error: parameter for %s is already present in forcefield." % smirks )
+                else:
+                    ff.addParameter( params, smirks, force_section[idx], tag[idx] )
+
+                # Increment parameter id
+                param_id_by_section[name] +=1
+            # If it's a torsion, check to see if there are already parameters and
+            # if so, add a new term to this torsion
             else:
-                # Otherwise, just store new parameters
-                ff.addParameter( params, smirks, force_section[idx], tag[idx])
-                # Increment parameter ID
-                param_id_by_section[name] += 1
+                # If we have parameters already
+                oldparams = ff.getParameter(smirks, force_type=force_section[idx])
+                if oldparams:
+                    # Find what number to use
+                    idnr = 1
+                    paramtag = 'k%s' % idnr
+                    while paramtag in params:
+                        idnr+=1
+                        paramtag = 'k%s' % idnr
+                    # Construct new param object with updated numbers
+                    for paramtag in ('periodicity1', 'phase1', 'idivf1', 'k1'):
+                        if paramtag in params:
+                            val = params.pop(paramtag)
+                            oldparams[paramtag[:-1]+str(idnr) ] = val
+                    # Store
+                    ff.setParameter( oldparams, smirks=smirks, force_type=force_section[idx])
+                else:
+                    # Otherwise, just store new parameters
+                    ff.addParameter( params, smirks, force_section[idx], tag[idx])
+                    # Increment parameter ID
+                    param_id_by_section[name] += 1
 
 
-# Write SMIRFF XML file
-ff.writeFile(outxml)
+    # Write SMIRFF XML file
+    ff.writeFile(outxml)
 
-# Roundtrip to fix formatting (for some reason etree won't format it properly on first write after modification)
-tmp = ForceField(outxml)
-tmp.writeFile(outxml)
+    # Roundtrip to fix formatting (for some reason etree won't format it properly on first write after modification)
+    tmp = ForceField(outxml)
+    tmp.writeFile(outxml)
+
+
+if __name__=="__main__":
+    from optparse import OptionParser
+    usage_string="""\
+    Convert specified SMIRKS-ified AMBER frcmod file into SMIRFF FFXML format, inserting converted parameters into a template FFXML file and writing to a new output file.
+
+    usage: convert.py --frcmod test.frcmod --template template.ffxml --xml test.ffxml
+    """
+    parser = OptionParser(usage=usage_string)
+
+    parser.add_option('-f', '--frcmod', type = "string", dest='infile', default = None, action="store", help="Name of input smirks-ified frcmod file.")
+    parser.add_option('-t', '--template', type="string", dest='inxml', default = None, action ="store", help="Name of template SMIRFF ffxml file.")
+    parser.add_option('-o', '--xml', type="string", dest='outxml', default =None, action="store", help="Name of output SMIRFF ffxml file.")
+    (options,args) = parser.parse_args()
+
+    if (options.infile is None) or (options.inxml is None) or (options.outxml is None):
+        parser.print_help()
+        parser.error("Input frcmod and template files and output FFXML file must be specified.")
+
+
+    convert_frcmod_to_ffxml( options.infile, options.inxml, options.outxml )
