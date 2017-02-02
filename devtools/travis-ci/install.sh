8c17eb77d448ddbc1d115e57cc684e198b86b0f7
Caitlin Bannan <bannanc@users.noreply.github.com>
2017-02-02 13:08:27 -0800

    try specific version

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -17,10 +17,9 @@ bash $MINICONDA -b -p $MINICONDA_HOME
 export PIP_ARGS="-U"
 export PATH=$MINICONDA_HOME/bin:$PATH
 conda update --yes conda
-conda install --yes conda-build jinja2 anaconda-client pip
+conda install --yes conda-build jinja2 anaconda-client pip matplotlib pandas=0.17.0
 conda install --yes -c omnia openmoltools
 conda install --yes -c omnia parmed
-conda install --yes -c matplotlib
 
 # Restore original directory
 popd
