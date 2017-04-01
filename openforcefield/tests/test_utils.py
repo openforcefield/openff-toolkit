6847b20aee879661b9268b4ab697dd245ce83dd1
John Chodera <john.chodera@choderalab.org>
2017-04-01 12:18:03 -0700

    Remove odds file tests

--- openforcefield/tests/test_utils.py
+++ openforcefield/tests/test_utils.py
@@ -6,10 +6,6 @@ from openforcefield import utils
 class TestUtils(TestCase):
     def test_read_molecules(self):
         molecules = utils.read_molecules('zinc-subset-tripos.mol2.gz', verbose=False)
-    def test_parse_odds_file(self):
-        odds = utils.parse_odds_file('odds_files/atom_index_odds.smarts')
-        odds = utils.parse_odds_file('odds_files/bond_OR_bases.smarts')
-        self.assertIsNone(odds[1], msg = "Parsing odds file with no odds should give None as the second entry")
     def test_positions(self):
         """Test ability to extract and set positions."""
         molecules = utils.read_molecules('zinc-subset-tripos.mol2.gz', verbose=False)
