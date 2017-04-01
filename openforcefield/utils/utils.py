ed08baead9dfb12a9b41a3e815b0499e4f13ca93
John Chodera <john.chodera@choderalab.org>
2017-04-01 09:34:46 -0700

    Fix data path

--- openforcefield/utils/utils.py
+++ openforcefield/utils/utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Utility subroutines for SMARTY atom type sampling
+Utility subroutines for open forcefield tools
 
 """
 #=============================================================================================
@@ -73,7 +73,7 @@ def generateTopologyFromOEMol(molecule):
 def get_data_filename(relative_path):
     """Get the full path to one of the reference files in testsystems.
 
-    In the source distribution, these files are in ``smarty/data/``,
+    In the source distribution, these files are in ``openforcefield/data/``,
     but on installation, they're moved to somewhere in the user's python
     site-packages directory.
 
@@ -85,7 +85,7 @@ def get_data_filename(relative_path):
     """
 
     from pkg_resources import resource_filename
-    fn = resource_filename('smarty', os.path.join('data', relative_path))
+    fn = resource_filename('openforcefield', os.path.join('data', relative_path))
 
     if not os.path.exists(fn):
         raise ValueError("Sorry! %s does not exist. If you just added it, you'll have to re-install" % fn)
