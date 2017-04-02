f019dfe5ea210edfb2b5a8dc7a34f5a4f13172ad
John Chodera <john.chodera@choderalab.org>
2017-04-01 18:28:04 -0700

    Clean up examples.

--- examples/SMIRNOFF_simulation/run_molecule.py
+++ examples/SMIRNOFF_simulation/run_molecule.py
@@ -1,27 +1,32 @@
 #!/bin/env python
 
 import time
-import simtk.openmm as mm
-from simtk.openmm import app
-from simtk.openmm import Platform
-from simtk.unit import *
 import numpy as np
+
+# Import OpenMM tools
+from simtk import openmm, unit
+from simtk.openmm import app, Platform
+
+# Use MDTraj to write simulation trajectories
 from mdtraj.reporters import NetCDFReporter
 
-from openforcefield.utils import *
+# Import the SMIRNOFF forcefield engine and some useful tools
 from openforcefield.typing.engines.smirnoff import ForceField
+from openforcefield.utils import get_data_filename, extractPositionsFromOEMol, generateTopologyFromOEMol
 
-#Define what molecule to work on, and a few simulation parameters
-molname = 'AlkEthOH_r51'
-mol_filename =  molname+'.mol2'
-time_step = 2 #Femtoseconds
-temperature = 300 #kelvin
-friction = 1 # per picosecond
-num_steps = 100000
-trj_freq = 1000 #steps
-data_freq = 1000 #steps
+# Import the OpenEye toolkit
+from openeye import oechem
 
-# Load OEMol
+# Define what molecule to work on, and a few simulation parameters
+mol_filename = 'AlkEthOH_r51.mol2'
+time_step = 2*unit.femtoseconds # simulation timestep
+temperature = 300*unit.kelvin # simulation temperature
+friction = 1/unit.picosecond # collision rate
+num_steps = 100000 # number of steps to run
+trj_freq = 1000 # number of steps per written trajectory frame
+data_freq = 1000 # number of steps per written simulation statistics
+
+# Load molecule using OpenEye tools
 mol = oechem.OEGraphMol()
 ifs = oechem.oemolistream(mol_filename)
 flavor = oechem.OEIFlavor_Generic_Default | oechem.OEIFlavor_MOL2_Default | oechem.OEIFlavor_MOL2_Forcefield
@@ -29,34 +34,27 @@ ifs.SetFlavor( oechem.OEFormat_MOL2, flavor)
 oechem.OEReadMolecule(ifs, mol )
 oechem.OETriposAtomNames(mol)
 
-# Get positions
-coordinates = mol.GetCoords()
-natoms = len(coordinates)
-positions = np.zeros([natoms,3], np.float32)
-for index in range(natoms):
-    (x,y,z) = coordinates[index]
-    positions[index,0] = x
-    positions[index,1] = y
-    positions[index,2] = z
-positions = Quantity(positions, unit.angstroms)
+# Get positions in OpenMM-compatible format
+positions = extractPositionsFromOEMol(mol)
 
-# Load forcefield
+# Load a SMIRNOFF small molecule forcefield for alkanes, ethers, and alcohols
 forcefield = ForceField(get_data_filename('forcefield/Frosst_AlkEtOH_parmAtFrosst.ffxml'))
 
-#Define system
+# Create the OpenMM system
 topology = generateTopologyFromOEMol(mol)
 system = forcefield.createSystem(topology, [mol])
 
-#Do simulation
-integrator = mm.LangevinIntegrator(temperature*kelvin, friction/picoseconds, time_step*femtoseconds)
-platform = mm.Platform.getPlatformByName('Reference')
+# Set up an OpenMM simulation
+integrator = openmm.LangevinIntegrator(temperature, friction, time_step)
+platform = openmm.Platform.getPlatformByName('Reference')
 simulation = app.Simulation(topology, system, integrator)
 simulation.context.setPositions(positions)
-simulation.context.setVelocitiesToTemperature(temperature*kelvin)
+simulation.context.setVelocitiesToTemperature(temperature)
 netcdf_reporter = NetCDFReporter('trajectory.nc', trj_freq)
 simulation.reporters.append(netcdf_reporter)
 simulation.reporters.append(app.StateDataReporter('data.csv', data_freq, step=True, potentialEnergy=True, temperature=True, density=True))
 
+# Run the simulation
 print("Starting simulation")
 start = time.clock()
 simulation.step(num_steps)
