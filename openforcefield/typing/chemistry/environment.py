9654057b5118998cb3440b397f1669d40ce8cf22
bannanc <bannanc@uci.edu>
2018-04-19 21:06:03 -0700

    fix aromatic atoms in 'no-bracketed' atoms, but still have extra print statements from debugging

--- openforcefield/typing/chemistry/environment.py
+++ openforcefield/typing/chemistry/environment.py
@@ -79,6 +79,7 @@ def _convert_embedded_SMIRKS(smirks):
     new_smirks = _convert_embedded_SMIRKS(initial_smirks)
     # new_smirks = [#1:1]~[#6]
     """
+    print('input smirks', smirks)
     a_out = 0
     while smirks.find('$(') != -1:
         # Find first atom
@@ -97,7 +98,9 @@ def _convert_embedded_SMIRKS(smirks):
 
         # Check for ring index, i.e. the 1s in "[#6:1]1-CCCCC1"
         match = re.match(r'(\d+)',post_smirks)
+        print('looking for a ring in embedded', post_smirks)
         if match is not None: # leftover starts with int
+            print('Found ring in embedded conversion', post_smirks)
             ring = re.findall(r'(\d+)',post_smirks)[0]
             leftover = post_smirks[match.end():]
         else:
@@ -122,6 +125,7 @@ def _convert_embedded_SMIRKS(smirks):
         # Make new smirks
         smirks = pre_smirks+new_atom+ring+embedded+post_smirks
 
+    print('returning smirks', smirks)
     return smirks
 
 def _remove_blanks_repeats(init_list, remove_list = ['']):
@@ -431,6 +435,7 @@ class ChemicalEnvironment(object):
         chirality = "!?[@]\d+|!?[@]@?"
 
         # Generate RegEx string for decorators:
+        self.no_bracket_atom_reg = r'('+'|'.join([element_sym, aro_sym, replace_str])+')'
         self.atom_reg = '|'.join([element_num, aro_ali, needs_int,
             optional_int, chirality, replace_str, element_sym, aro_sym])
         self.atom_reg = r'('+self.atom_reg+')'
@@ -547,8 +552,7 @@ into ChemicalEnvironments." % smirks)
                 start_string = smirks
 
             # Check for atoms not between square brackets
-            reg = r'(\$\w+|[A-Z][a-z]?)'
-            split = re.split(reg, start_string)
+            split = re.split(self.no_bracket_atom_reg, start_string)
             atom_string = split[1]
 
             # update leftover for this condition
@@ -565,6 +569,7 @@ into ChemicalEnvironments." % smirks)
         # Check for ring index, i.e. the 1s in "[#6:1]1-CCCCC1"
         match = re.match(r'(\d+)',leftover)
         if match is not None: # leftover starts with int
+            print("Found a ring in ", leftover)
             ring = re.findall(r'(\d+)',leftover)[0]
             leftover = leftover[match.end():]
         else:
@@ -577,6 +582,7 @@ into ChemicalEnvironments." % smirks)
         atoms[idx] = new_atom
 
         while len(leftover) > 0:
+            print(leftover)
             idx += 1
 
             # Check for branching
@@ -599,10 +605,10 @@ into ChemicalEnvironments." % smirks)
                 bond_string = leftover
 
             # Check for atoms not between square brackets
-            reg = r'(\$\w+|[A-Z][a-z]?)'
-            bond_split = re.split(reg, bond_string)
+            bond_split = re.split(self.no_bracket_atom_reg, bond_string)
             # Next atom is not in brackets for example C in "[#7:1]-C"
             if len(bond_split) > 1:
+                print('found no bracket atom', bond_split)
                 bond_string = bond_split[0]
                 atom_string = '['+bond_split[1]+']'
                 # update leftover for this condition
