4b334da6bd02ad2acda1ad939e8a277c2873223b
SimonBoothroyd <simon.boothroyd@choderalab.org>
2019-01-18 11:18:16 -0500

    Removed test data. Fixed missing entries in layer and backend init files.

--- openforcefield/properties/estimator/backends/__init__.py
+++ openforcefield/properties/estimator/backends/__init__.py
@@ -1,1 +1,2 @@
+from .base import PropertyEstimatorBackend
 from .dask import DaskLocalClusterBackend
--- openforcefield/properties/estimator/layers/__init__.py
+++ openforcefield/properties/estimator/layers/__init__.py
@@ -1,4 +1,4 @@
-from .base import available_layers
+from .base import available_layers, register_calculation_layer, PropertyCalculationLayer
 from .simulation import SimulationLayer
 from .reweighting import ReweightingLayer
 from .surrogate import SurrogateLayer
