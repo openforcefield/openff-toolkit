9c787edd8b060f4b7387a2341a71fea057c84ced
Andrea Rizzi <andrrizzi@gmail.com>
2019-03-22 01:09:02 +0100

    Fix test_util_serialization and convert to pytest

--- openforcefield/tests/test_utils_serialization.py
+++ openforcefield/tests/test_utils_serialization.py
@@ -13,7 +13,8 @@ Tests for utility methods for serialization
 # GLOBAL IMPORTS
 #=============================================================================================
 
-from unittest import TestCase
+import pytest
+
 
 #=============================================================================================
 # TESTS
@@ -22,6 +23,7 @@ from unittest import TestCase
 from openforcefield.utils.serialization import Serializable
 
 class Thing(Serializable):
+
     def __init__(self, description, mylist):
         self.description = description
         self.mylist = mylist
@@ -30,18 +32,18 @@ class Thing(Serializable):
         return {
             'description' : self.description,
             'mylist' : self.mylist
-            }
+        }
 
-    @staticmethod
-    def from_dict(d):
-        return Thing(d['description'], d['mylist'])
+    @classmethod
+    def from_dict(cls, d):
+        return cls(d['description'], d['mylist'])
 
     def __eq__(self, other):
         """Comparator for asserting object field equality."""
-        equality = True
-        equality == equality and (self.description == other.description)
-        equality == equality and (self.mylist == other.mylist)
+        equality = self.description == other.description
+        equality &= self.mylist == other.mylist
         return equality
+
 
 # DEBUG
 def write(filename, contents):
@@ -54,62 +56,62 @@ def write(filename, contents):
     with open(filename, mode) as outfile:
         outfile.write(contents)
 
-class TestUtilsSerialization(TestCase):
+
+class TestUtilsSerialization:
     """Test serialization and deserialization of a simple class."""
 
-    def setUp(self):
-        # Create an example object
-        self.thing = Thing('blorb', [1,2,3])
-        self.thing_class = Thing
+    @classmethod
+    def setup_class(cls):
+        cls.thing = Thing('blorb', [1, 2, 3])
 
     def test_json(self):
         """Test JSON serialization"""
         json_thing = self.thing.to_json()
-        write('out.json', json_thing) # DEBUG
-        thing_from_json = self.thing_class.from_json(json_thing)
+        thing_from_json = self.thing.__class__.from_json(json_thing)
         assert self.thing == thing_from_json
 
     def test_yaml(self):
         """Test YAML serialization"""
         yaml_thing = self.thing.to_yaml()
-        write('out.yaml', yaml_thing) # DEBUG
-        thing_from_yaml = self.thing_class.from_yaml(yaml_thing)
+        thing_from_yaml = self.thing.__class__.from_yaml(yaml_thing)
         assert self.thing == thing_from_yaml
 
     def test_bson(self):
         """Test BSON serialization"""
         bson_thing = self.thing.to_bson()
-        write('out.bson', bson_thing) # DEBUG
-        thing_from_bson = self.thing_class.from_bson(bson_thing)
+        thing_from_bson = self.thing.__class__.from_bson(bson_thing)
         assert self.thing == thing_from_bson
 
+    @pytest.mark.wip(reason=("the current implementation of to_toml cannot handle dict "
+                             "keys associated to None (e.g. the ToolkitAM1BCC tag in the "
+                             "TestUtilsSMIRNOFFSerialization suite)."))
     def test_toml(self):
         """Test TOML serialization"""
         toml_thing = self.thing.to_toml()
-        write('out.toml', toml_thing) # DEBUG
-        thing_from_toml = self.thing_class.from_toml(toml_thing)
+        thing_from_toml = self.thing.__class__.from_toml(toml_thing)
         assert self.thing == thing_from_toml
 
     def test_messagepack(self):
         """Test MessagePack serialization"""
         messagepack_thing = self.thing.to_messagepack()
-        write('out.messagepack', messagepack_thing) # DEBUG
-        thing_from_messagepack = self.thing_class.from_messagepack(messagepack_thing)
+        thing_from_messagepack = self.thing.__class__.from_messagepack(messagepack_thing)
         assert self.thing == thing_from_messagepack
 
+    @pytest.mark.wip(reason='from/to_xml is not implemented yet. This test fails '
+                            'because the list of integers is saved in the XML as '
+                            'a list of numeric strings.')
     def test_xml(self):
         """Test XML serialization"""
         xml_thing = self.thing.to_xml()
-        write('out.xml', xml_thing) # DEBUG
-        thing_from_xml = self.thing_class.from_xml(xml_thing)
+        thing_from_xml = self.thing.__class__.from_xml(xml_thing)
         assert self.thing == thing_from_xml
 
     def test_pickle(self):
         """Test pickle serialization"""
         pkl_thing = self.thing.to_pickle()
-        write('out.pkl', pkl_thing) # DEBUG
-        thing_from_pkl = self.thing_class.from_pickle(pkl_thing)
+        thing_from_pkl = self.thing.__class__.from_pickle(pkl_thing)
         assert self.thing == thing_from_pkl
+
 
 class DictionaryContainer(Serializable):
     def __init__(self, dictionary):
@@ -127,10 +129,13 @@ class DictionaryContainer(Serializable):
         """Comparator for asserting object field equality."""
         return self.dictionary == other.dictionary
 
+
 class TestUtilsSMIRNOFFSerialization(TestUtilsSerialization):
     """Test serialization and deserialization of smirnoff99Frosst."""
 
-    def setUp(self):
+    @classmethod
+    def setup_class(cls):
+        cls.thing = Thing('blorb', [1, 2, 3])
         # Create an example object holding the SMIRNOFF xmltodict dictionary representation
         import xmltodict
         from openforcefield.utils import get_data_filename
@@ -138,5 +143,4 @@ class TestUtilsSMIRNOFFSerialization(TestUtilsSerialization):
         with open(filename, 'r') as f:
             xml = f.read()
             dictionary = xmltodict.parse(xml)
-            self.thing = DictionaryContainer(dictionary)
-            self.thing_class = DictionaryContainer
+            cls.thing = DictionaryContainer(dictionary)
