f8ab9340e005378d0df22a378cbb935d410338f9
John Chodera <john.chodera@choderalab.org>
2019-04-08 13:19:58 -0400

    Import specific classes in __init__ to avoid polluting namespace

--- openforcefield/topology/__init__.py
+++ openforcefield/topology/__init__.py
@@ -1,2 +1,5 @@
-from openforcefield.topology.molecule import *
-from openforcefield.topology.topology import *
+from openforcefield.topology.molecule import Particle, Atom, Bond, \
+    VirtualSite, BondChargeVirtualSite, MonovalentLonePairVirtualSite, DivalentLonePairVirtualSite, TrivalentLonePairVirtualSite, \
+    FrozenMolecule, Molecule
+from openforcefield.topology.topology import DuplicateUniqueMoleculeError, NotBondedError, \
+    ValenceDict, ImproperDict, TopologyAtom, TopologyBond, TopologyVirtualSite, TopologyMolecule, Topology
