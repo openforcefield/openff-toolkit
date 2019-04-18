cc94841d20732d16037e90b9d76a2752fc73580b
Andrea Rizzi <andrrizzi@gmail.com>
2019-04-18 17:12:03 -0400

    Fix #279: Warning raised in __init__ if openeye not installed

--- openforcefield/__init__.py
+++ openforcefield/__init__.py
@@ -1,9 +1,3 @@
-try:
-    import openeye
-except Exception as e:
-    print(e)
-    print('Warning: Cannot import openeye toolkit; not all functionality will be available.')
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
