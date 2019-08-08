cf44b71e9415d62ffffbe4a5f01aad7e65863a92
Jeff Wagner <jwagnerjpl@gmail.com>
2019-08-08 11:36:14 -0700

    loosen energy tolerance for GBSA energy tests

--- openforcefield/tests/test_forcefield.py
+++ openforcefield/tests/test_forcefield.py
@@ -1277,7 +1277,7 @@ class TestForceFieldParameterAssignment:
 
 
         # Ensure that the GBSA energies (which we put into ForceGroup 1) are identical
-        assert (amber_energy[1] - off_energy[1]) < 1e-6 * unit.kilojoule/unit.mole
+        assert (amber_energy[1] - off_energy[1]) < 1e-4 * unit.kilojoule/unit.mole
 
         # Ensure that all system energies are the same
         compare_system_energies(off_omm_system, amber_omm_system, positions, by_force_type=False)
@@ -1403,7 +1403,7 @@ class TestForceFieldParameterAssignment:
         # print(openmm.XmlSerializer.serialize(amber_gbsa_force))
 
         # Ensure that the GBSA energies (which we put into ForceGroup 1) are identical
-        assert (amber_energy[1] - off_energy[1]) < 1e-6 * unit.kilojoule/unit.mole
+        assert (amber_energy[1] - off_energy[1]) < 1e-4 * unit.kilojoule/unit.mole
 
         # If charges are zero, the GB energy component should be 0, so the total GBSA energy should be 0
         if zero_charges:
