3790f9f833f403b445625debc8a2f4dec7313c8b
Jeff Wagner <jwagnerjpl@gmail.com>
2019-09-18 18:06:35 -0700

    Fix typo in before_install, don't specify xenial

--- devtools/travis-ci/before_install.sh
+++ devtools/travis-ci/before_install.sh
@@ -28,8 +28,8 @@ bash $MINICONDA -b -p $MINICONDA_HOME
 # Configure miniconda
 export PIP_ARGS="-U"
 # New to conda >=4.4
-conda config --add channels openeye		echo ". $MINICONDA_HOME/etc/profile.d/conda.sh" >> ~/.bashrc  # Source the profile.d file
-conda config --add channels omnia		echo "conda activate" >> ~/.bashrc  # Activate conda
+echo ". $MINICONDA_HOME/etc/profile.d/conda.sh" >> ~/.bashrc  # Source the profile.d file
+echo "conda activate" >> ~/.bashrc  # Activate conda
 source ~/.bashrc  # source file to get new commands
 conda config --add channels openeye
 conda config --add channels omnia
