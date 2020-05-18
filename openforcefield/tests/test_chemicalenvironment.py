9da930b57b3fdd46b93d2402ebd8e15eee3e4a57
Jeff Wagner <jwagnerjpl@gmail.com>
2020-05-18 16:40:40 -0700

    refactor smirks parsing test, add ring cases

--- openforcefield/tests/test_chemicalenvironment.py
+++ openforcefield/tests/test_chemicalenvironment.py
@@ -1,13 +1,17 @@
 from functools import partial
 from openforcefield.typing.chemistry import *
 from openforcefield.utils import get_data_file_path
-from unittest import TestCase
+#from unittest import TestCase
 import pytest
 from openforcefield.utils.toolkits import OPENEYE_AVAILABLE
 
+if OPENEYE_AVAILABLE:
+    toolkits = ['rdkit', 'openeye']
+else:
+    toolkits = ['rdkit']
 # TODO: Bring these back online once OpenEye dependence is resolved
-@pytest.mark.skip
-class TestChemicalEnvironments(TestCase):
+#@pytest.mark.skip
+class TestChemicalEnvironments():#TestCase):
     def test_createEnvironments(self):
         """
         Test all types of ChemicalEnvironment objects with defined atoms and bonds
@@ -76,37 +80,49 @@ class TestChemicalEnvironments(TestCase):
         removed = torsion.removeAtom(atom3beta1)
         self.assertTrue(removed, f"failed to remove atom ({atom3beta1.asSMIRKS()}) in {torsion.asSMIRKS()} which is removeable")
 
-    def test_parseSMIRKS(self):
+    @pytest.mark.parametrize(['smirks', 'expected_valence', 'expected_chemenv_class'],
+                             [["[#6](-[#1])-[#8]", None, ChemicalEnvironment],
+                              ["[#6&X4&H0:1](-[#1])-[#6&X4]", 'Atom', AtomChemicalEnvironment],
+                              ["[#6&X4&H0:1](-[#1])-[#6&X4:2]", 'Bond', BondChemicalEnvironment],
+                              ["[*:1]-[*:2](-[#6&X4])-[*:3]", 'Angle', AngleChemicalEnvironment],
+                              ["[#6&X4&H0:1](-[#1])-[#6&X4:2]-[#6&X4&H0:3](-[#1])-[#6&X4:4]", 'ProperTorsion',
+                               TorsionChemicalEnvironment],
+                              ["[#1:1]-[#6&X4:2](-[#8:3])-[#1:4]", 'ImproperTorsion', ImproperChemicalEnvironment],
+                              ["[#1:1]-[#6&X4:2](-[#8:3])-[*:4](-[#6&H1])-[#8:5]", None, ChemicalEnvironment],
+                              ["[#6$(*~[#6]=[#8])$(*-,=[$ewg2,#7])]", None, ChemicalEnvironment],
+                              ["CCC", None, ChemicalEnvironment],
+                              ["[#6:1]1(-;!@[#1,#6])=;@[#6]-;@[#6]1", 'Atom', ChemicalEnvironment],
+                              ["C(O-[#7,#8])CC=[*]", None, ChemicalEnvironment],
+                              ["[#6$([#6X4](~[$ewg1])(~[#8]~[#1])):1]-[#6X2H2;+0:2]-,=,:;!@;!#[$ewg2:3]-[#4:4]",
+                               'ProperTorsion', TorsionChemicalEnvironment],
+                              ["[#6$([#6X4](~[$ewg1])(~[#8]~[#1])):1]1=CCCC1", 'Atom', AtomChemicalEnvironment],
+                              ["[*:1]-[#7X3:2](-[#6a$(*1ccc(-[#8-1X1])cc1):3])-[*:4]", 'ImproperTorsion',
+                               ImproperChemicalEnvironment],
+                              ["[#6X4:1]1~[*:2]~[*$(*~[#1]):3]1", 'Angle', AngleChemicalEnvironment],
+                              ["[$([#7]1~[#6]-CC1)]", None, ChemicalEnvironment],
+                              ["[$(c1ccccc1)]", None, ChemicalEnvironment],
+                              # The next two tests are for ring-closing bonds
+                              ["[H][C@:4]1(C(C([C:3]([N:2]1[C:1](=O)C([H])([H])[H])([H])[H])([H])[H])([H])[H])C=O",
+                               'ImproperTorsion', ChemicalEnvironment],
+                              ["[P:1]=1=[P]=[P]=[P]=[P:2]=1", 'Bond', BondChemicalEnvironment],
+                              ]
+                             )
+    @pytest.mark.parametrize('toolkit', toolkits)
+    def test_parseSMIRKS(self, toolkit, smirks, expected_valence, expected_chemenv_class):
         """
         Test creating environments with SMIRKS
         """
         replacements = [ ('ewg1', '[#7!-1,#8!-1,#16!-1,#9,#17,#35,#53]'),
                 ('ewg2', '[#7!-1,#8,#16]') ]
 
-        smirksList = [ ["[#6](-[#1])-[#8]", None, ChemicalEnvironment],
-                ["[#6&X4&H0:1](-[#1])-[#6&X4]", 'Atom', AtomChemicalEnvironment],
-                [ "[#6&X4&H0:1](-[#1])-[#6&X4:2]", 'Bond', BondChemicalEnvironment],
-                [ "[*:1]-[*:2](-[#6&X4])-[*:3]", 'Angle', AngleChemicalEnvironment],
-                [ "[#6&X4&H0:1](-[#1])-[#6&X4:2]-[#6&X4&H0:3](-[#1])-[#6&X4:4]", 'ProperTorsion', TorsionChemicalEnvironment],
-                [ "[#1:1]-[#6&X4:2](-[#8:3])-[#1:4]", 'ImproperTorsion', ImproperChemicalEnvironment],
-                [ "[#1:1]-[#6&X4:2](-[#8:3])-[*:4](-[#6&H1])-[#8:5]", None, ChemicalEnvironment],
-                [ "[#6$(*~[#6]=[#8])$(*-,=[$ewg2,#7])]", None, ChemicalEnvironment],
-                [ "CCC", None, ChemicalEnvironment],
-                [ "[#6:1]1(-;!@[#1,#6])=;@[#6]-;@[#6]1", 'Atom', ChemicalEnvironment],
-                [ "C(O-[#7,#8])CC=[*]", None, ChemicalEnvironment],
-                [ "[#6$([#6X4](~[$ewg1])(~[#8]~[#1])):1]-[#6X2H2;+0:2]-,=,:;!@;!#[$ewg2:3]-[#4:4]", 'ProperTorsion', TorsionChemicalEnvironment],
-                [ "[#6$([#6X4](~[$ewg1])(~[#8]~[#1])):1]1=CCCC1", 'Atom', AtomChemicalEnvironment],
-                [ "[*:1]-[#7X3:2](-[#6a$(*1ccc(-[#8-1X1])cc1):3])-[*:4]", 'ImproperTorsion', ImproperChemicalEnvironment],
-                [ "[#6X4:1]1~[*:2]~[*$(*~[#1]):3]1", 'Angle', AngleChemicalEnvironment],
-                [ "[$([#7]1~[#6]-CC1)]", None, ChemicalEnvironment],
-                [ "[$(c1ccccc1)]", None, ChemicalEnvironment],
-                ]
-
-        for toolkit in ['openeye', 'rdkit']:
-            for [smirks, checkType, chemEnv] in smirksList:
-                env = chemEnv(smirks = smirks, replacements = replacements, toolkit=toolkit)
-                Type = env.getType()
-                self.assertEqual(Type, checkType, f"SMIRKS ({smirks}) clasified as {Type} instead of {checkType} using {toolkit} toolkit")
+        #smirksList =
+        #for toolkit in ['openeye', 'rdkit']:
+        #for [smirks, checkType, chemEnv] in smirksList:
+        env = expected_chemenv_class(smirks = smirks, replacements = replacements, toolkit=toolkit)
+        actual_type = env.getType()
+        #self.assertEqual(Type, checkType, f"SMIRKS ({smirks}) classified as {Type} instead of {checkType} using {toolkit} toolkit")
+        assert actual_type == expected_valence, \
+               f"SMIRKS ({smirks}) classified as {actual_type} instead of {expected_valence} using {toolkit} toolkit"
 
     def test_environment_functions(self):
         """
