80b45d82770b4e24c878eb9a29273fadf6a02b05
David Dotson <dotsdl@gmail.com>
2020-05-27 18:33:58 -0700

    Monkeypatching to keep m2r working

--- docs/conf.py
+++ docs/conf.py
@@ -313,20 +313,19 @@ texinfo_documents = [
 
 
 # workaround for m2r broken on sphinx >= 3.x
-#def monkeypatch(cls):
-#    """ decorator to monkey-patch methods """
-#    def decorator(f):
-#        method = f.__name__
-#        old_method = getattr(cls, method)
-#        setattr(cls, method, lambda self, *args, **kwargs: f(old_method, self, *args, **kwargs))
-#    return decorator
-#
-## workaround until https://github.com/miyakogi/m2r/pull/55 is merged
-#@monkeypatch(sphinx.registry.SphinxComponentRegistry)
-#def add_source_parser(_old_add_source_parser, self, *args, **kwargs):
-#    # signature is (parser: Type[Parser], **kwargs), but m2r expects
-#    # the removed (str, parser: Type[Parser], **kwargs).
-#    if isinstance(args[0], str):
-#        args = args[1:]
-#    return _old_add_source_parser(self, *args, **kwargs)
-#
+def monkeypatch(cls):
+    """ decorator to monkey-patch methods """
+    def decorator(f):
+        method = f.__name__
+        old_method = getattr(cls, method)
+        setattr(cls, method, lambda self, *args, **kwargs: f(old_method, self, *args, **kwargs))
+    return decorator
+
+# workaround until https://github.com/miyakogi/m2r/pull/55 is merged
+@monkeypatch(sphinx.registry.SphinxComponentRegistry)
+def add_source_parser(_old_add_source_parser, self, *args, **kwargs):
+    # signature is (parser: Type[Parser], **kwargs), but m2r expects
+    # the removed (str, parser: Type[Parser], **kwargs).
+    if isinstance(args[0], str):
+        args = args[1:]
+    return _old_add_source_parser(self, *args, **kwargs)
