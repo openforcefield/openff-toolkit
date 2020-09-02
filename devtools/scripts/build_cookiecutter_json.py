33f8b23516214364032e153c53fb5584e7f8e65a
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2020-09-02 13:10:39 -0500

    Make installers only on the specified platform and Python version

--- devtools/scripts/build_cookiecutter_json.py
+++ devtools/scripts/build_cookiecutter_json.py
@@ -2,11 +2,22 @@ import sys
 import json
 
 
-data = {'name': 'openforcefield',
- 'channel': 'omnia',
- 'python': ['3.6', '3.7'],
- 'platform': ['linux-64', 'osx-64'],
- 'release': sys.argv[1]}
+release_tag = sys.argv[1]
+python_version = sys.argv[2]
+ci_os = sys.argv[3]
 
-with open('new_cookiecutter.json', 'w') as fp:
+platform_mapping = {
+    "ubuntu-latest": "linux-64",
+    "macOS-latest": "osx-64",
+}
+
+data = {
+    "name": "openforcefield",
+    "channel": "omnia",
+    "python": python_version,
+    "platform": platform_mapping[ci_os],
+    "release": release_tag,
+}
+
+with open("new_cookiecutter.json", "w") as fp:
     json.dump(data, fp)
