f23059e14ef278687db92b74a19645907749b547
Matthew W. Thompson <mattwthompson@protonmail.com>
2021-02-09 14:32:32 -0600

    Move 1-4 parsing into get_14_scaling_factors

--- openff/toolkit/tests/test_forcefield.py
+++ openff/toolkit/tests/test_forcefield.py
@@ -26,6 +26,7 @@ from numpy.testing import assert_almost_equal
 from simtk import openmm, unit
 
 from openff.toolkit.tests.utils import (
+    get_14_scaling_factors,
     requires_openeye,
     requires_openeye_mol2,
     requires_rdkit,
@@ -4224,36 +4225,21 @@ class TestForceFieldParameterAssignment:
             [e_mod_omm_sys, 0.5, 0.66],
             [vdw_mod_omm_sys, 0.777, 0.833333],
         ]:
-            nonbond_force = [
-                f for f in omm_sys.getForces() if type(f) == openmm.NonbondedForce
-            ][0]
-            for exception_idx in range(nonbond_force.getNumExceptions()):
-                i, j, q, sig, eps = nonbond_force.getExceptionParameters(exception_idx)
+            found_coul_14, found_vdw_14 = get_14_scaling_factors(omm_sys)
 
-                # Trust that q == 0 covers the cases of 1-2, 1-3, and truly being 0
-                if q / unit.elementary_charge ** 2 != 0:
-                    q_i = nonbond_force.getParticleParameters(i)[0]
-                    q_j = nonbond_force.getParticleParameters(j)[0]
-                    coul_14 = q / (q_i * q_j)
+            np.testing.assert_almost_equal(
+                actual=found_vdw_14,
+                desired=expected_vdw_14,
+                decimal=10,
+                err_msg="vdW 1-4 scaling factors do not match",
+            )
 
-                    np.testing.assert_almost_equal(
-                        actual=coul_14,
-                        desired=expected_coul_14,
-                        decimal=10,
-                        err_msg="Electrostatics 1-4 scaling factors do not match",
-                    )
-
-                if eps / unit.kilojoule_per_mole != 0:
-                    eps_i = nonbond_force.getParticleParameters(i)[2]
-                    eps_j = nonbond_force.getParticleParameters(j)[2]
-                    vdw_14 = eps / (eps_i * eps_j) ** 0.5
-
-                    np.testing.assert_almost_equal(
-                        actual=vdw_14,
-                        desired=expected_vdw_14,
-                        decimal=10,
-                        err_msg="vdW 1-4 scaling factors do not match",
-                    )
+            np.testing.assert_almost_equal(
+                actual=found_coul_14,
+                desired=expected_coul_14,
+                decimal=10,
+                err_msg="Electrostatics 1-4 scaling factors do not match",
+            )
 
     @requires_openeye
     def test_overwrite_bond_orders(self):
