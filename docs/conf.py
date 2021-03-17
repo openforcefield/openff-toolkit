c8768d69181363e23014a1f7dafeba8bcb286b52
Josh Mitchell <yoshanuikabundi@gmail.com>
2021-03-17 19:45:31 +1100

    Added custom CSS for floating table of contents

--- docs/conf.py
+++ docs/conf.py
@@ -230,6 +230,12 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'styles.css',
+]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
