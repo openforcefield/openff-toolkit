f7ff63c063c3de08f7097b27b96f8fd3cb082c86
Josh Mitchell <yoshanuikabundi@gmail.com>
2021-03-22 15:20:48 +1100

    Remove custom stylesheet

--- docs/conf.py
+++ docs/conf.py
@@ -230,12 +230,6 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# These paths are either relative to html_static_path
-# or fully qualified paths (eg. https://...)
-html_css_files = [
-    'styles.css',
-]
-
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
