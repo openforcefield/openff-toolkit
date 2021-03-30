d98be7735d14d38eb342fa1632d21ff3bbd6e657
Josh Mitchell <yoshanuikabundi@gmail.com>
2021-03-30 21:06:13 +1100

    Cleanup and lint

--- utilities/examples_helper/cli.py
+++ utilities/examples_helper/cli.py
@@ -1,25 +1,22 @@
-import click
+import shutil
+import subprocess as sp
+import sys
+from pathlib import Path
 
+import click
 import conda.cli.python_api
 import conda.exceptions
 
-
-from pathlib import Path
-import shutil
-from itertools import cycle
-import subprocess as sp
-import sys
-
 OFFTK_ROOT = (Path(__file__) / "../../../").resolve()
-EXAMPLES_DIR = (OFFTK_ROOT / "examples")
-EXAMPLES_ENV = (OFFTK_ROOT / "devtools/conda-envs/examples.yaml")
+EXAMPLES_DIR = OFFTK_ROOT / "examples"
+EXAMPLES_ENV = OFFTK_ROOT / "devtools/conda-envs/examples.yaml"
 
 
 def flatten(iterable):
     return (item for inner in iterable for item in inner)
 
 
-def echo(*objects, sep=' ', end='\n', file=sys.stdout, color=None):
+def echo(*objects, sep=" ", end="\n", file=sys.stdout, color=None):
     """click.echo(), but like print()"""
     message = sep.join((str(object) for object in objects)) + end
 
@@ -33,11 +30,7 @@ def conda_env(cmd, *args, **kwargs):
     incorrectly consumes arguments from the parent process, rather than from
     the run_command() call.
     """
-    kwargs = dict(
-        stdin="STRING",
-        stdout="STRING",
-        use_exception_handler=False
-    )
+    kwargs = dict(stdin="STRING", stdout="STRING", use_exception_handler=False)
     kwargs.update(kwargs)
 
     if "search_path" in kwargs:
@@ -72,7 +65,6 @@ def conda_cmd(cmd, *args, **kwargs):
             return (None, None, 0)
 
 
-
 def get_current_conda_prefix():
     """Return the currently active conda environment
 
@@ -84,7 +76,7 @@ def get_current_conda_prefix():
         if line.startswith("#"):
             continue
         name, _, prefix = line.partition(" * ")
-        if (prefix):
+        if prefix:
             return Path(prefix.strip())
     raise ValueError("Could not determine prefix")
 
@@ -110,9 +102,7 @@ class Example:
         self.path = Path(path).resolve()
 
         if not self.path.is_dir():
-            raise ValueError(
-                "path should be a directory"
-            )
+            raise ValueError("path should be a directory")
 
         # `list(path.glob("*.ipynb"))` doesn't work here
         notebooks = [p for p in self.path.glob("*.ipynb")]
@@ -120,9 +110,7 @@ class Example:
         if len(notebooks) == 1:
             self.notebook = notebooks[0].relative_to(self.path)
         else:
-            raise ValueError(
-                "path should be a folder with exactly one notebook"
-            )
+            raise ValueError("path should be a folder with exactly one notebook")
 
         if (self.path / "environment.yaml").is_file():
             self.environment = self.path / "environment.yaml"
@@ -141,6 +129,7 @@ class Example:
 
 class ExampleArg(click.ParamType):
     """Construct an `Example` from a click argument"""
+
     def convert(self, value, param, ctx):
         """
         Takes a path or name and looks in `EXAMPLES_DIR` or the working
@@ -168,7 +157,7 @@ class ExampleArg(click.ParamType):
             f"Could not find example {value}. It should be a folder "
             f"containing exactly one Jupyter notebook.",
             param,
-            ctx
+            ctx,
         )
 
 
@@ -187,8 +176,8 @@ def style_path(value):
 def style_cmd(value):
     return (
         "\n"
-        + style(" $ ", dim=True, bg='black')
-        + style(value, bold=True, bg='black')
+        + style(" $ ", dim=True, bg="black")
+        + style(value, bold=True, bg="black")
         + "\n"
     )
 
@@ -213,29 +202,46 @@ def list():
 
 @main.command()
 @click.argument("example", type=ExampleArg())
-@click.option("-t", "--target", type=click.Path(), default=Path('.'),
-              help="Target directory in which to install the example.")
-@click.option("-d", "--dry-run", default=False, is_flag=True,
-              help="Describe what we're doing, but take no action.")
-@click.option("-q", "--quiet", default=False, is_flag=True,
-              help="Produce less text output.")
-@click.option("-u", "--update-current", default=False, is_flag=True,
-              help="Update the current Conda environment, rather than creating a new one.")
+@click.option(
+    "-t",
+    "--target",
+    type=click.Path(),
+    default=Path("."),
+    help="Target directory in which to install the example.",
+)
+@click.option(
+    "-d",
+    "--dry-run",
+    default=False,
+    is_flag=True,
+    help="Describe what we're doing, but take no action.",
+)
+@click.option(
+    "-q", "--quiet", default=False, is_flag=True, help="Produce less text output."
+)
+@click.option(
+    "-u",
+    "--update-current",
+    default=False,
+    is_flag=True,
+    help="Update the current Conda environment, rather than creating a new one.",
+)
 def install(example, target, dry_run, quiet, update_current):
     """Copy an example to a convenient location."""
-    target = (target / example.name)
+    target = target / example.name
 
     if dry_run:
         echo(style("Dry run; taking no file system actions.", fg="red"))
 
     if not quiet:
-        echo(f"Taking example {style_path(example.name)} from {style_path(example.path)}")
+        echo(
+            f"Taking example {style_path(example.name)} from {style_path(example.path)}"
+        )
         echo(f"Installing to {style_path(target)}")
 
     if target.exists():
         raise click.BadParameter(
-            f"Directory {style_path(target)} already exists",
-            param_hint="--target"
+            f"Directory {style_path(target)} already exists", param_hint="--target"
         )
     if not dry_run:
         shutil.copytree(example.path, target)
@@ -252,60 +258,55 @@ def install(example, target, dry_run, quiet, update_current):
             if not quiet:
                 echo(f"Creating new conda environment at prefix {style_path(prefix)}")
 
-            conda_cmd(
-                "create",
-                "--prefix", prefix,
-                *conda_args
-            )
+            conda_cmd("create", "--prefix", prefix, *conda_args)
         else:
             prefix = get_current_conda_prefix()
 
-        environments = [
-            EXAMPLES_ENV,
-            example.environment
-        ]
+        environments = [EXAMPLES_ENV, example.environment]
         environments = [("--file", e) for e in environments if e is not None]
         if not quiet:
-            echo(f"Installing dependencies from the following files into environment at {style_path(prefix)}:")
+            echo(
+                f"Installing dependencies from the following files into environment at {style_path(prefix)}:"
+            )
             for (_, path) in environments:
                 echo(f"    {style_path(path)}")
         environments = flatten(environments)
 
         conda_cmd(
-            "env", "update",
-            "--prefix", prefix,
+            "env",
+            "update",
+            "--prefix",
+            prefix,
             *environments,
             *conda_args,
             # Pass through stdout and stderr
-            stdout=None, stderr=None
+            stdout=None,
+            stderr=None,
         )
 
         if dry_run:
-            echo(style('Dry run complete.', fg='red'))
+            echo(style("Dry run complete.", fg="red"))
         else:
             echo(
-                style('Installation complete.', fg='green'),
+                style("Installation complete.", fg="green"),
                 f"Activate the example Conda environment with",
                 style_cmd(f"conda activate {prefix.resolve()}"),
                 f"With the environment active and working from the new "
                 f"{style_path(target)} directory, run the example with",
-                style_cmd(
-                    f"jupyter notebook {example.notebook}"
-                ),
+                style_cmd(f"jupyter notebook {example.notebook}"),
                 f"To get started right now:",
                 style_cmd(
                     f"conda run -p {prefix} --cwd {target} jupyter notebook {example.notebook}"
                 ),
-                sep="\n"
+                sep="\n",
             )
     except Exception as e:
         if not dry_run:
             echo(
                 style(
-                    "Exception raised; cleaning up installation in progress",
-                    fg="red"
+                    "Exception raised; cleaning up installation in progress", fg="red"
                 ),
-                file=sys.stderr
+                file=sys.stderr,
             )
             shutil.rmtree(target)
         raise e
