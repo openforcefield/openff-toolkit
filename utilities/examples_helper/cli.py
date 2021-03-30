b941d43bcfb6f10b7191685599d2cb850a2b0336
Josh Mitchell <yoshanuikabundi@gmail.com>
2021-03-30 20:05:23 +1100

    Add conda environment management to example tool

--- utilities/examples_helper/cli.py
+++ utilities/examples_helper/cli.py
@@ -1,10 +1,85 @@
 import click
-from click import echo
+
+import conda.cli.python_api as conda
+
 
 from pathlib import Path
 import shutil
+from itertools import cycle
+import subprocess as sp
+import sys
 
-EXAMPLES_DIR = (Path(__file__) / "../../../examples").resolve()
+OFFTK_ROOT = (Path(__file__) / "../../../").resolve()
+EXAMPLES_DIR = (OFFTK_ROOT / "examples")
+EXAMPLES_ENV = (OFFTK_ROOT / "devtools/conda-envs/examples.yaml")
+
+
+def flatten(iterable):
+    return (item for inner in iterable for item in inner)
+
+
+def echo(*objects, sep=' ', end='\n', file=sys.stdout, color=None):
+    """click.echo(), but like print()"""
+    message = sep.join((str(object) for object in objects)) + end
+
+    return click.echo(message, file=file, nl=False, err=False, color=color)
+
+
+def conda_env(cmd, *args, **kwargs):
+    """Run `conda env`; mimics signature of `conda.cli.python_api.run_command()`
+
+    This function is necessary because `conda env` run through run_command()
+    incorrectly consumes arguments from the parent process, rather than from
+    the run_command() call.
+    """
+    kwargs = dict(
+        stdin="STRING",
+        stdout="STRING",
+        use_exception_handler=False
+    )
+    kwargs.update(kwargs)
+
+    if "search_path" in kwargs:
+        raise ValueError("conda env does not support search_path")
+
+    if kwargs["stdin"] == "STRING":
+        del kwargs["stdin"]
+        kwargs["text"] = True
+    if kwargs["stdout"] == "STRING":
+        del kwargs["stdout"]
+        kwargs["text"] = True
+    if not kwargs["use_exception_handler"]:
+        del kwargs["use_exception_handler"]
+        kwargs["check"] = True
+
+    proc = sp.run(["conda", "env", cmd, *args], **kwargs)
+
+    return (proc.stdout, proc.stderr, proc.returncode)
+
+
+def conda_cmd(cmd, *args, **kwargs):
+    """Wrapper around both run_command() and conda_env()"""
+    args = [str(arg) for arg in args]
+    if cmd == "env":
+        return conda_env(*args, **kwargs)
+    else:
+        return conda.run_command(cmd, *args, **kwargs)
+
+
+def get_current_conda_prefix():
+    """Return the currently active conda environment
+
+    Hacky as hell, but Conda doesn't seem to provide a robust solution:
+    https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#determining-your-current-environment
+    """
+    stdout, stderr, code = conda_cmd("info", "--envs")
+    for line in stdout.splitlines():
+        if line.startswith("#"):
+            continue
+        name, _, prefix = line.partition(" * ")
+        if (prefix):
+            return Path(prefix.strip())
+    raise ValueError("Could not determine prefix")
 
 
 class Example:
@@ -20,24 +95,33 @@ class Example:
         exactly one Jupyter notebook (*.ipynb). The path to this
         notebook is available as `Example.notebook`. A human and
         machine readable name of the `Example` is taken from the
-        directory's path and is available as `Example.name`
+        directory's path and is available as `Example.name`. If a file
+        named environment.yml or environment.yaml exists in the
+        directory, it is available as `Example.environment`
         """
         self.path = Path(path).resolve()
 
-        if self.path.is_dir():
-            # `list(path.glob("*.ipynb"))` doesn't work here
-            notebooks = [p for p in self.path.glob("*.ipynb")]
+        if not self.path.is_dir():
+            raise ValueError(
+                "path should be a directory"
+            )
 
-            if len(notebooks) == 1:
-                self.notebook = notebooks[0]
-            else:
-                raise ValueError(
-                    "path should be a folder with exactly one notebook"
-                )
+        # `list(path.glob("*.ipynb"))` doesn't work here
+        notebooks = [p for p in self.path.glob("*.ipynb")]
+
+        if len(notebooks) == 1:
+            self.notebook = notebooks[0]
         else:
             raise ValueError(
                 "path should be a folder with exactly one notebook"
             )
+
+        if (self.path / "environment.yaml").is_file():
+            self.environment = self.path / "environment.yaml"
+        elif (self.path / "environment.yml").is_file():
+            self.environment = self.path / "environment.yml"
+        else:
+            self.environment = None
 
     def __str__(self):
         return f"Example('{self.path}')"
@@ -80,6 +164,27 @@ class ExampleArg(click.ParamType):
         )
 
 
+def style(value, *args, **kwargs):
+    return click.style(str(value), *args, **kwargs)
+
+
+def strong(value):
+    return style(value, bold=True)
+
+
+def style_path(value):
+    return strong(value)
+
+
+def style_cmd(value):
+    return (
+        "\n"
+        + style(" $ ", dim=True, bg='black')
+        + style(value, bold=True, bg='black')
+        + "\n"
+    )
+
+
 @click.group()
 def main():
     """Easily run examples with the Open Force Field Toolkit."""
@@ -104,18 +209,86 @@ def list():
               help="Target directory in which to install the example.")
 @click.option("-d", "--dry-run", default=False, is_flag=True,
               help="Describe what we're doing, but take no action.")
-def install(example, target, dry_run):
+@click.option("-q", "--quiet", default=False, is_flag=True,
+              help="Produce less text output.")
+@click.option("-u", "--update-current", default=False, is_flag=True,
+              help="Update the current Conda environment, rather than creating a new one.")
+def install(example, target, dry_run, quiet, update_current):
     """Copy an example to a convenient location."""
-    target = (target / example.name).resolve()
+    target = (target / example.name)
 
     if dry_run:
         echo("Dry run; taking no file system actions")
 
-    echo(f"Sourcing '{example.name}' from {example.path}")
-    echo(f"Installing to {target}")
+    if not quiet:
+        echo(f"Taking example {style_path(example.name)} from {style_path(example.path)}")
+        echo(f"Installing to {style_path(target)}")
 
     if not dry_run:
         shutil.copytree(example.path, target)
+
+    conda_args = []
+    if dry_run:
+        conda_args.append("--dry-run")
+    if quiet:
+        conda_args.append("--quiet")
+
+    try:
+        if not update_current:
+            prefix = target / ".env"
+            if not quiet:
+                echo(f"Creating new conda environment at prefix {style_path(prefix)}")
+
+            conda_cmd(
+                "create",
+                "--prefix", prefix,
+                *conda_args
+            )
+        else:
+            prefix = get_current_conda_prefix()
+
+        environments = [
+            EXAMPLES_ENV,
+            example.environment
+        ]
+        environments = [("--file", e) for e in environments if e is not None]
+        if not quiet:
+            echo(f"Installing dependencies from the following files into environment at {style_path(prefix)}:")
+            for (_, path) in environments:
+                echo(f"    {style_path(path)}")
+        environments = flatten(environments)
+
+        conda_cmd(
+            "env", "update",
+            "--prefix", prefix,
+            *environments,
+            *conda_args,
+            # Pass through stdout and stderr
+            stdout=None, stderr=None
+        )
+
+        echo(
+            style('Installation complete.', fg='green'),
+            f"From the new {style_path(target)} directory, activate the "
+            f"example prefix with",
+            style_cmd(f"conda activate {prefix.resolve()}"),
+            f"and run the example with",
+            style_cmd(
+                f"jupyter notebook {example.notebook.relative_to(example.path)}"
+            ),
+            sep="\n"
+        )
+    except Exception as e:
+        if not dry_run:
+            echo(
+                style(
+                    "Exception raised; cleaning up installation in progress",
+                    fg="red"
+                ),
+                file=sys.stderr
+            )
+            shutil.rmtree(target)
+        raise e
 
 
 if __name__ == "__main__":
