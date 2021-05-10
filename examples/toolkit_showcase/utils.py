51295036fc6277dd9b61660ab062eb6717edf92a
Josh Mitchell <yoshanuikabundi@gmail.com>
2021-05-10 17:03:17 +1000

    Use openmmforcefields to combine parameters, not ParmEd
    Using ommff simplifies the showcase substantially without
    loss of function. With this commit, we also use the
    constrained Parsley force field, reducing hidden magic
    and increasing relevance. Prose and descriptions are not
    yet updated. GROMACS now only requires `-maxwarn 1`, which
    is due to a known openFF toolkit bug.

--- examples/toolkit_showcase/utils.py
+++ examples/toolkit_showcase/utils.py
@@ -5,6 +5,35 @@ import nglview
 import mdtraj as mdt
 
 
+def off_topology_to_omm(off_topology):
+    """Convert openff.toolkit.topology.Topology to simtk.openmm.app.topology.Topology"""
+    omm_topology = openmm.app.topology.Topology()
+    chain = omm_topology.addChain()
+    res = omm_topology.addResidue("LIG", chain)
+    atoms = {}
+
+    for atom in off_topology.topology_atoms:
+        atom_name = (
+            atom.atom.name
+            if atom.atom.name
+            else str(atom.element.symbol) + str(atom.topology_atom_index)
+        )
+
+        atoms[atom.topology_atom_index] = omm_topology.addAtom(
+            atom_name, atom.element, res
+        )
+
+    for bond in off_topology.topology_bonds:
+        (a, b) = bond.atoms
+        omm_topology.addBond(
+            atoms[a.topology_atom_index],
+            atoms[b.topology_atom_index],
+            order=bond.bond_order,
+        )
+
+    return omm_topology
+
+
 def find_clashing_water(pmd_struct, lig_resname, distance=0.15):
     """
     Find waters that are sterically clashing with a ligand.
