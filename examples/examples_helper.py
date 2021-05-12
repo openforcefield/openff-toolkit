e2ade6ddcb0e3fd51cc3135bab1dda13e231e48e
Josh Mitchell <yoshanuikabundi@gmail.com>
2021-05-12 15:36:32 +1000

    Initial simple implementation of the helper

--- examples/examples_helper.py
+++ examples/examples_helper.py
@@ -1,1 +1,18 @@
-1/0
+#!/usr/bin/env python3
+"""Copy the openff-toolkit examples suite to a local directory"""
+
+from pathlib import Path
+from os import environ
+from shutil import copytree
+
+
+def main():
+    """Entrypoint for the script"""
+    examples_path = Path(environ["CONDA_PREFIX"]) / "share/openff-toolkit/examples"
+    target_path = Path("./examples")
+
+    copytree(examples_path, target_path)
+
+
+if __name__ == "__main__":
+    main()
