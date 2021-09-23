d39905b488850483c77de782db643af1a7ed1fb9
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2021-09-23 11:55:46 -0700

    Speed up rdkit distances (#1070)
    * replace rdkit distances
    
    * add branch to ci
    
    * small modifications
    
    * blacken and remove CI
    
    * add comments and change variable name
    
    Co-authored-by: Matt Thompson <mattwthompson@protonmail.com>

--- openff/toolkit/utils/rdkit_wrapper.py
+++ openff/toolkit/utils/rdkit_wrapper.py
@@ -1111,25 +1111,33 @@ class RDKitToolkitWrapper(base_wrapper.ToolkitWrapper):
         ).reshape(-1, 1)
 
         # Build an exclusion list for 1-2 and 1-3 interactions.
-        excluded_pairs = {
-            *[(bond.atom1_index, bond.atom2_index) for bond in molecule.bonds],
-            *[
-                (angle[0].molecule_atom_index, angle[-1].molecule_atom_index)
-                for angle in molecule.angles
-            ],
-        }
+        excluded_x, excluded_y = zip(
+            *{
+                *[(bond.atom1_index, bond.atom2_index) for bond in molecule.bonds],
+                *[
+                    (angle[0].molecule_atom_index, angle[-1].molecule_atom_index)
+                    for angle in molecule.angles
+                ],
+            }
+        )
 
         # Build the distance matrix between all pairs of atoms.
         coordinates = conformer.value_in_unit(unit.angstrom)
 
-        distances = np.sqrt(
-            np.sum(np.square(coordinates)[:, np.newaxis, :], axis=2)
-            - 2 * coordinates.dot(coordinates.T)
-            + np.sum(np.square(coordinates), axis=1)
-        )
+        # Equation 1: (a - b)^2 = a^2 - 2ab + b^2
+        # distances_squared will eventually wind up as the squared distances
+        # although it is first computed as the ab portion of Eq 1
+        distances_squared = coordinates.dot(coordinates.T)
+        # np.einsum is both faster than np.diag, and not read-only
+        # we know that a^2 == b^2 == diag(ab)
+        diag = np.einsum("ii->i", distances_squared)
+        # we modify in-place so we can use the `diag` view
+        # to make the diagonals 0
+        distances_squared += distances_squared - diag - diag[..., np.newaxis]
         # Handle edge cases where the squared distance is slightly negative due to
         # precision issues
-        np.fill_diagonal(distances, 0.0)
+        diag[:] = -0.0  # this is somewhat faster than np.fill_diagonal
+        distances = np.sqrt(-distances_squared)
 
         inverse_distances = np.reciprocal(
             distances, out=np.zeros_like(distances), where=~np.isclose(distances, 0.0)
@@ -1138,9 +1146,8 @@ class RDKitToolkitWrapper(base_wrapper.ToolkitWrapper):
         # Multiply by the charge products.
         charge_products = partial_charges @ partial_charges.T
 
-        for x, y in excluded_pairs:
-            charge_products[x, y] = 0.0
-            charge_products[y, x] = 0.0
+        charge_products[excluded_x, excluded_y] = 0.0
+        charge_products[excluded_y, excluded_x] = 0.0
 
         interaction_energies = inverse_distances * charge_products
 
