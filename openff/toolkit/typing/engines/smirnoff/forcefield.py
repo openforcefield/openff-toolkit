390e7b92a183f6bae4aae891bf5e88e6d1588d3d
Matt Thompson <mattwthompson@protonmail.com>
2022-01-06 11:12:16 -0600

    Port #1156
    * Remove deprecated exceptions
    
    * Update release history
    
    * Remove some unused imports
    
    * Fix broken import

--- openff/toolkit/typing/engines/smirnoff/forcefield.py
+++ openff/toolkit/typing/engines/smirnoff/forcefield.py
@@ -31,7 +31,6 @@ import copy
 import logging
 import os
 import pathlib
-import warnings
 from collections import OrderedDict
 from typing import TYPE_CHECKING, List, Tuple, Union
 
@@ -57,28 +56,6 @@ from openff.toolkit.utils.utils import (
     convert_all_strings_to_quantity,
     requires_package,
 )
-
-if TYPE_CHECKING:
-    import openmm
-
-    from openff.toolkit.topology import Topology
-
-deprecated_names = ["ParseError"]
-
-
-def __getattr__(name):
-    if name in deprecated_names:
-        warnings.filterwarnings("default", category=DeprecationWarning)
-        warning_msg = f"{name} is DEPRECATED and will be removed in a future release of the OpenFF Toolkit."
-        warnings.warn(warning_msg, DeprecationWarning)
-
-        if name == "ParseError":
-            from openff.toolkit.utils.exceptions import _DeprecatedParseError
-
-            return _DeprecatedParseError
-
-    raise AttributeError(f"module {__name__} has no attribute {name}")
-
 
 # =============================================================================================
 # CONFIGURE LOGGER
