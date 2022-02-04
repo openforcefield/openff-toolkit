a3852064cf6f91978fa127e705c42edf55106f32
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-02-04 10:20:21 -0600

    Add some unit tests for mm_molecule.py

---
+++ openff/toolkit/tests/test_mm_molecule.py
@@ -0,0 +1,75 @@
+import pytest
+
+from openff.toolkit.topology.mm_molecule import _TypedAtom, _TypedBond, _TypedMolecule
+from openff.toolkit.topology.molecule import Molecule
+
+
+class TestMMMolecule:
+    @pytest.fixture()
+    def water(self):
+        water = _TypedMolecule()
+        water.add_atom(atomic_number=8)
+        water.add_atom(atomic_number=1)
+        water.add_atom(atomic_number=1)
+        water.add_bond(0, 1)
+        water.add_bond(0, 2)
+
+        return water
+
+    @pytest.fixture()
+    def molecule_with_bogus_atom(self):
+        molecule = _TypedMolecule()
+        molecule.add_atom(atomic_number=6)
+        molecule.add_atom(atomic_number=0)
+        molecule.add_bond(0, 1)
+
+        return molecule
+
+    def test_create_water(self):
+        water = _TypedMolecule()
+        water.add_atom(atomic_number=8)
+        water.add_atom(atomic_number=1)
+        water.add_atom(atomic_number=1)
+        water.add_bond(0, 1)
+        water.add_bond(0, 2)
+        assert water.n_atoms == 3
+        assert water.n_particles == 3
+        assert water.n_bonds == 2
+        assert water.n_conformers == 0
+
+    def test_bond_getters(self, water):
+        assert water.get_bond_between(0, 1) is not water.get_bond_between(0, 2)
+        assert water.bond(0) is not water.bond(1)
+
+        assert water.get_bond_between(0, 1) is water.bond(0)
+        assert water.get_bond_between(0, 2) is water.bond(1)
+
+    def test_hill_formula(self, water, molecule_with_bogus_atom):
+
+        assert water.hill_formula == "H2O"
+        assert molecule_with_bogus_atom.hill_formula == "INVALID"
+
+    def test_dict_roundtrip(self, water):
+        roundtrip = _TypedMolecule.from_dict(water.to_dict())
+
+        assert roundtrip.n_atoms == water.n_atoms
+        assert roundtrip.n_bonds == water.n_bonds
+
+        for atom_index in range(roundtrip.n_atoms):
+            assert (
+                roundtrip.atom(atom_index).atomic_number
+                == water.atom(atom_index).atomic_number
+            )
+
+    def test_from_molecule(self):
+        converted = _TypedMolecule.from_molecule(Molecule.from_smiles("O"))
+
+        assert converted.n_atoms == 3
+        assert converted.n_bonds == 2
+
+        expected_atomic_numbers = [8, 1, 1]
+        for atom_index in range(converted.n_atoms):
+            assert (
+                converted.atom(atom_index).atomic_number
+                == expected_atomic_numbers[atom_index]
+            )
