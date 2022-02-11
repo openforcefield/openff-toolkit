be42146c442b12d8080d9650eb4c84389800d586
Josh Mitchell <yoshanuikabundi@gmail.com>
2022-02-11 15:05:11 +1100

    Move top level re-exports to new app module

--- openff/toolkit/__init__.py
+++ openff/toolkit/__init__.py
@@ -6,23 +6,3 @@ A modern, extensible library for molecular mechanics force field science from th
 from ._version import get_versions  # type: ignore
 
 __version__ = get_versions()["version"]
-
-import openff.toolkit.topology as topology
-import openff.toolkit.typing as typing
-import openff.toolkit.utils as utils
-from openff.toolkit.topology import Molecule, Topology
-from openff.toolkit.typing.engines.smirnoff.forcefield import (
-    ForceField,
-    get_available_force_fields,
-)
-
-__all__ = [
-    "__version__",
-    "topology",
-    "typing",
-    "utils",
-    "Molecule",
-    "Topology",
-    "ForceField",
-    "get_available_force_fields",
-]
---
+++ openff/toolkit/app.py
@@ -0,0 +1,12 @@
+from openff.toolkit.topology import Molecule, Topology
+from openff.toolkit.typing.engines.smirnoff.forcefield import (
+    ForceField,
+    get_available_force_fields,
+)
+
+__all__ = [
+    "Molecule",
+    "Topology",
+    "ForceField",
+    "get_available_force_fields",
+]
