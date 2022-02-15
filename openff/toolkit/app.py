ed96e05c979ecfe3cee836202f898ad55221e7e6
Josh Mitchell <yoshanuikabundi@gmail.com>
2022-02-15 13:48:09 +1100

    Add toolkits to app module

--- openff/toolkit/app.py
+++ openff/toolkit/app.py
@@ -3,10 +3,24 @@ from openff.toolkit.typing.engines.smirnoff.forcefield import (
     ForceField,
     get_available_force_fields,
 )
+from openff.toolkit.utils.toolkits import (
+    GLOBAL_TOOLKIT_REGISTRY,
+    AmberToolsToolkitWrapper,
+    BuiltInToolkitWrapper,
+    OpenEyeToolkitWrapper,
+    RDKitToolkitWrapper,
+    ToolkitRegistry,
+)
 
 __all__ = [
     "Molecule",
     "Topology",
     "ForceField",
     "get_available_force_fields",
+    "GLOBAL_TOOLKIT_REGISTRY",
+    "AmberToolsToolkitWrapper",
+    "BuiltInToolkitWrapper",
+    "OpenEyeToolkitWrapper",
+    "RDKitToolkitWrapper",
+    "ToolkitRegistry",
 ]
