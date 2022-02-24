e414a523a1791897f5d41d98ab336a4f5dafdca5
Josh A. Mitchell <yoshanuikabundi@gmail.com>
2022-02-24 11:22:23 +1100

    Port #1171
    https://github.com/openforcefield/openff-toolkit/commit/b1d8e7e1a2db4e6fc267afe47e734f29965b5508
    
    Fix trans COOh in Elf10 conformer selection (#1171)
    
    * Add clear error message to openeye apply_elf_conformer_selection in silent failures
    
    * Fix apply_elf_conformer_selection when specifying rdkit wrapper
    
    * Add test for Molecule.apply_elf_conformer_selection with toolkit wrapper
    
    * Add Molecule._make_carboxylic_acids_cis method + tests
    
    * Make COOH cis when generating conformers in assign_partial_charges with openeye toolkit
    
    * Make COOH cis when generating conformers in assign_fractional_bond_orders with openeye toolkit and an ELF10 method
    
    * Update changelog
    
    * Typo in release notes
    
    * Update docs/releasehistory.md
    
    Co-authored-by: Jeff Wagner <jwagnerjpl@gmail.com>
    
    * Improve robustness of make_carboxylic_acids_cis tests
    
    * Remove two safety checks from molecule.py now that they're in test_molecule.py
    
    * Add make_carboxylic_acids_cis switch to rdkit and molecule generate_conformers()
    
    * Add toolkit_registry argument to Molecule._make_carboxylic_acids_cis()
    
    * Remove erroneous comment from _make_carboxylic_acids_cis
    
    * _make_carboxylic_acids_cis: Exit early if molecule has no COOH groups
    
    * Make OE assign methods consistent
    
    * Update openff/toolkit/topology/molecule.py
    
    Co-authored-by: SimonBoothroyd <simon.boothroyd@hotmail.com>
    
    * Switch make_carboxylic_acids_cis test to formic acid
    
    Clarifies test by using simpler molecule and using dummy values to lay out the molecule obviously
    
    Co-authored-by: SimonBoothroyd <simon.boothroyd@hotmail.com>
    
    * Lint and clean up
    
    * Simplify test_make_carboxylic_acids_cis
    
    * generate_conformers makes COOH cis by default
    
    Co-authored-by: Jeff Wagner <jwagnerjpl@gmail.com>
    Co-authored-by: SimonBoothroyd <simon.boothroyd@hotmail.com>

--- openff/toolkit/utils/rdkit_wrapper.py
+++ openff/toolkit/utils/rdkit_wrapper.py
@@ -848,7 +848,13 @@ class RDKitToolkitWrapper(base_wrapper.ToolkitWrapper):
         return molecule
 
     def generate_conformers(
-        self, molecule, n_conformers=1, rms_cutoff=None, clear_existing=True, _cls=None
+        self,
+        molecule,
+        n_conformers=1,
+        rms_cutoff=None,
+        clear_existing=True,
+        _cls=None,
+        make_carboxylic_acids_cis=False,
     ):
         r"""
         Generate molecule conformers using RDKit.
@@ -875,6 +881,9 @@ class RDKitToolkitWrapper(base_wrapper.ToolkitWrapper):
             Whether to overwrite existing conformers for the molecule.
         _cls : class
             Molecule constructor
+        make_carboxylic_acids_cis: bool, default=False
+            Guarantee all conformers have exclusively cis carboxylic acid groups (COOH)
+            by rotating the proton in any trans carboxylic acids 180 degrees around the C-O bond.
 
         """
         from rdkit.Chem import AllChem
@@ -904,6 +913,9 @@ class RDKitToolkitWrapper(base_wrapper.ToolkitWrapper):
 
         for conformer in molecule2._conformers:
             molecule._add_conformer(conformer)
+
+        if make_carboxylic_acids_cis:
+            molecule._make_carboxylic_acids_cis(toolkit_registry=self)
 
     def assign_partial_charges(
         self,
