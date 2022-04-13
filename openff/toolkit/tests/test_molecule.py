Fixes for units changes

openff/toolkit/tests/test_molecule.py
@@ -3665,7 +3665,7 @@ class TestMolecule:
         # First, check that we get exactly the right coords for cis and trans methanoic acid
         offmol = Molecule.from_mapped_smiles("[H:1][C:2](=[O:3])[O:4][H:5]")
         # cis methanoic (formic) acid
-        cis_xyz = (
+        cis_xyz = unit.Quantity(
             np.array(
                 [
                     [-1.0, -1.0, 0.0],  # HC
@@ -3674,10 +3674,10 @@ class TestMolecule:
                     [+1.0, +0.0, 0.0],  # -O
                     [+2.0, +1.0, 0.0],  # HO
                 ]
-            )
-            * unit.angstrom
+            ),
+            unit.angstrom,
         )
-        trans_xyz = (
+        trans_xyz = unit.Quantity(
             np.array(
                 [
                     [-1.0, -1.0, 0.0],  # HC
@@ -3686,8 +3686,8 @@ class TestMolecule:
                     [+1.0, +0.0, 0.0],  # -O
                     [+2.0, -1.0, 0.0],  # HO
                 ]
-            )
-            * unit.angstrom
+            ),
+            unit.angstrom,
         )
 
         offmol._conformers = [trans_xyz, cis_xyz]

openff/toolkit/topology/molecule.py
@@ -3520,9 +3520,7 @@ class FrozenMolecule(Serializable):
             return
 
         # Convert all conformers into one big array
-        conformers = np.asarray(
-            [q.value_in_unit(unit.angstrom) for q in self._conformers]
-        )
+        conformers = np.asarray([q.m_as(unit.angstrom) for q in self._conformers])
 
         # Scan the molecule for carboxylic acids
         cooh_indices = self.chemical_environment_matches(
@@ -3619,7 +3617,7 @@ class FrozenMolecule(Serializable):
         conformers[:, cooh_indices, :] = cooh_xyz
 
         # Return conformers to original type
-        self._conformers = [conf * unit.angstrom for conf in conformers]
+        self._conformers = [unit.Quantity(conf, unit.angstrom) for conf in conformers]
 
     def compute_virtual_site_positions_from_conformer(self, conformer_idx):
         """