d43e92622ff47798996563a3f5fbf837068b86b8
Matt Thompson <mattwthompson@protonmail.com>
2022-04-13 17:32:35 -0500

    Port #1171 (#1259)
    * Port #1171
    
    https://github.com/openforcefield/openff-toolkit/commit/b1d8e7e1a2db4e6fc267afe47e734f29965b5508
    
    Fix trans COOh in Elf10 conformer selection (#1171)
    
    * Add clear error message to openeye apply_elf_conformer_selection in silent failures
    
    * Fix apply_elf_conformer_selection when specifying rdkit wrapper
    
    * Add test for Molecule.apply_elf_conformer_selection with toolkit wrapper
    
    * Add Molecule._make_carboxylic_acids_cis method + tests
    
    * Make COOH cis when generating conformers in assign_partial_charges with openeye toolkit
    
    * Make COOH cis when generating conformers in assign_fractional_bond_orders with openeye toolkit and an ELF10 method
    
    * Update changelog
    
    * Typo in release notes
    
    * Update docs/releasehistory.md
    
    Co-authored-by: Jeff Wagner <jwagnerjpl@gmail.com>
    
    * Improve robustness of make_carboxylic_acids_cis tests
    
    * Remove two safety checks from molecule.py now that they're in test_molecule.py
    
    * Add make_carboxylic_acids_cis switch to rdkit and molecule generate_conformers()
    
    * Add toolkit_registry argument to Molecule._make_carboxylic_acids_cis()
    
    * Remove erroneous comment from _make_carboxylic_acids_cis
    
    * _make_carboxylic_acids_cis: Exit early if molecule has no COOH groups
    
    * Make OE assign methods consistent
    
    * Update openff/toolkit/topology/molecule.py
    
    Co-authored-by: SimonBoothroyd <simon.boothroyd@hotmail.com>
    
    * Switch make_carboxylic_acids_cis test to formic acid
    
    Clarifies test by using simpler molecule and using dummy values to lay out the molecule obviously
    
    Co-authored-by: SimonBoothroyd <simon.boothroyd@hotmail.com>
    
    * Lint and clean up
    
    * Simplify test_make_carboxylic_acids_cis
    
    * generate_conformers makes COOH cis by default
    
    Co-authored-by: Jeff Wagner <jwagnerjpl@gmail.com>
    Co-authored-by: SimonBoothroyd <simon.boothroyd@hotmail.com>
    
    * Manually update actions
    
    Bump upload-artifact
    
    * Fixes for units changes
    
    Co-authored-by: Josh A. Mitchell <yoshanuikabundi@gmail.com>
    Co-authored-by: Jeff Wagner <jwagnerjpl@gmail.com>
    Co-authored-by: SimonBoothroyd <simon.boothroyd@hotmail.com>

--- openff/toolkit/topology/molecule.py
+++ openff/toolkit/topology/molecule.py
@@ -3406,6 +3406,7 @@ class FrozenMolecule(Serializable):
         n_conformers=10,
         rms_cutoff=None,
         clear_existing=True,
+        make_carboxylic_acids_cis=True,
     ):
         """
         Generate conformers for this molecule using an underlying toolkit.
@@ -3425,6 +3426,11 @@ class FrozenMolecule(Serializable):
             for each ``ToolkitWrapper`` (generally 1 Angstrom).
         clear_existing : bool, default=True
             Whether to overwrite existing conformers for the molecule
+        make_carboxylic_acids_cis: bool, default=True
+            Guarantee all conformers have exclusively cis carboxylic acid groups (COOH)
+            by rotating the proton in any trans carboxylic acids 180 degrees around the
+            C-O bond. Works around a bug in conformer generation by the OpenEye toolkit
+            where trans COOH is much more common than it should be.
 
         Examples
         --------
@@ -3452,6 +3458,7 @@ class FrozenMolecule(Serializable):
                 rms_cutoff=rms_cutoff,
                 clear_existing=clear_existing,
                 raise_exception_types=[],
+                make_carboxylic_acids_cis=make_carboxylic_acids_cis,
             )
         elif isinstance(toolkit_registry, ToolkitWrapper):
             toolkit = toolkit_registry
@@ -3460,6 +3467,7 @@ class FrozenMolecule(Serializable):
                 n_conformers=n_conformers,
                 rms_cutoff=rms_cutoff,
                 clear_existing=clear_existing,
+                make_carboxylic_acids_cis=make_carboxylic_acids_cis,
             )
         else:
             raise InvalidToolkitRegistryError(
@@ -3467,6 +3475,149 @@ class FrozenMolecule(Serializable):
                     type(toolkit_registry)
                 )
             )
+
+    def _make_carboxylic_acids_cis(self, toolkit_registry=GLOBAL_TOOLKIT_REGISTRY):
+        """
+        Rotate dihedral angle of any conformers with trans COOH groups so they are cis
+
+        Carboxylic acid groups almost always exist in nature in the cis conformation,
+        with the hydrogen atom in between the two oxygen atoms::
+
+                  O----H
+                 /
+                /
+               /
+            --C
+               \\
+                \\
+                  O
+
+        However, the OpenEye toolkit frequently produces carboxylic acid geometries
+        in the unrealistic trans conformation::
+
+             H----O
+                 /
+                /
+               /
+            --C
+               \\
+                \\
+                  O
+
+        This method converts all conformers in the Molecule with the trans conformation
+        into the corresponding cis conformer by rotating the OH bond around the CO bond
+        by 180 degrees. Carboxylic acids that are already cis are unchanged. Carboxylic
+        acid groups are considered cis if their O-C-O-H dihedral angle is acute.
+
+        Parameters
+        ----------
+        toolkit_registry : openff.toolkit.utils.toolkits.ToolkitRegistry or openff.toolkit.utils.toolkits.ToolkitWrapper, optional, default=None
+            :class:`ToolkitRegistry` or :class:`ToolkitWrapper` to use for SMILES-to-molecule conversion
+        """
+
+        # Return early if there are no conformers
+        if not self._conformers:
+            return
+
+        # Convert all conformers into one big array
+        conformers = np.asarray([q.m_as(unit.angstrom) for q in self._conformers])
+
+        # Scan the molecule for carboxylic acids
+        cooh_indices = self.chemical_environment_matches(
+            "[C:2]([O:3][H:4])=[O:1]", toolkit_registry=toolkit_registry
+        )
+        n_conformers, n_cooh_groups = len(conformers), len(cooh_indices)
+        # Exit early if there are no carboxylic acids
+        if not n_cooh_groups:
+            return
+
+        # Pull out the coordinates of all carboxylic acid groups into cooh_xyz
+        cooh_xyz = conformers[:, cooh_indices, :]
+        assert cooh_xyz.shape == (n_conformers, n_cooh_groups, 4, 3)
+
+        def dot(a, b):
+            """Compute dot product along last axis of arrays"""
+            return np.sum(a * b, axis=-1)[..., np.newaxis]
+
+        def norm(a):
+            """Compute norm along last axis of array"""
+            return np.linalg.norm(a, axis=-1)[..., np.newaxis]
+
+        def dihedral(a):
+            """Compute dihedrals of array with shape (..., 4, 3)"""
+            # Praxeolitic formula
+            # 1 sqrt, 1 cross product
+            # from https://stackoverflow.com/questions/20305272/dihedral-torsion-angle-from-four-points-in-cartesian-coordinates-in-python
+            p0 = a[..., 0, :]
+            p1 = a[..., 1, :]
+            p2 = a[..., 2, :]
+            p3 = a[..., 3, :]
+
+            b0 = -1.0 * (p1 - p0)
+            b1 = p2 - p1
+            b2 = p3 - p2
+
+            # normalize b1 so that it does not influence magnitude of vector
+            # rejections that come next
+            b1 /= norm(b1)
+
+            # vector rejections
+            # v = projection of b0 onto plane perpendicular to b1
+            #   = b0 minus component that aligns with b1
+            # w = projection of b2 onto plane perpendicular to b1
+            #   = b2 minus component that aligns with b1
+            v = b0 - dot(b0, b1) * b1
+            w = b2 - dot(b2, b1) * b1
+
+            # angle between v and w in a plane is the torsion angle
+            # v and w may not be normalized but that's fine since tan is y/x
+            x = dot(v, w)
+            y = dot(np.cross(b1, v), w)
+            return np.arctan2(y, x)
+
+        dihedrals = dihedral(cooh_xyz)
+        assert dihedrals.shape == (n_conformers, n_cooh_groups, 1)
+        dihedrals.shape = (n_conformers, n_cooh_groups, 1, 1)
+
+        # Get indices of trans COOH groups
+        trans_indices = np.logical_not(
+            np.logical_and((-np.pi / 2) < dihedrals, dihedrals < (np.pi / 2))
+        )
+        # Expand array so it can be used to index cooh_xyz
+        trans_indices = np.repeat(trans_indices, repeats=4, axis=2)
+        trans_indices = np.repeat(trans_indices, repeats=3, axis=3)
+        # Get indices of individual atoms in trans COOH groups (except terminal O)
+        trans_indices_h = trans_indices.copy()
+        trans_indices_h[:, :, (0, 1, 2), :] = False
+        trans_indices_c = trans_indices.copy()
+        trans_indices_c[:, :, (0, 2, 3), :] = False
+        trans_indices_o = trans_indices.copy()
+        trans_indices_o[:, :, (0, 1, 3), :] = False
+
+        # Rotate OH around CO bond
+        # We want to rotate H 180 degrees around the CO bond (b1)
+        c = cooh_xyz[trans_indices_c].reshape(-1, 3)
+        o = cooh_xyz[trans_indices_o].reshape(-1, 3)
+        h = cooh_xyz[trans_indices_h].reshape(-1, 3)
+        # Axis is defined as the line from the origin along a unit vector, so
+        # move C to the origin and normalize
+        point = h - c
+        axis = o - c
+        axis /= norm(axis)
+        # Do the rotation
+        # https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
+        rotated = axis * (dot(axis, point)) - np.cross(np.cross(axis, point), axis)
+        # Move rotated point back to original coordinates
+        rotated = rotated + c
+
+        # Update the coordinates
+        cooh_xyz[trans_indices_h] = rotated.reshape((-1))
+
+        # Update conformers with rotated coordinates
+        conformers[:, cooh_indices, :] = cooh_xyz
+
+        # Return conformers to original type
+        self._conformers = [unit.Quantity(conf, unit.angstrom) for conf in conformers]
 
     def compute_virtual_site_positions_from_conformer(self, conformer_idx):
         """
@@ -3569,7 +3720,7 @@ class FrozenMolecule(Serializable):
         elif isinstance(toolkit_registry, ToolkitWrapper):
             toolkit = toolkit_registry
             toolkit.apply_elf_conformer_selection(  # type: ignore[attr-defined]
-                self, molecule=self, percentage=percentage, limit=limit, **kwargs
+                molecule=self, percentage=percentage, limit=limit, **kwargs
             )
         else:
             raise InvalidToolkitRegistryError(
