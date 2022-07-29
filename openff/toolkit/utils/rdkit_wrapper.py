fix for openff units

openff/toolkit/utils/rdkit_wrapper.py
@@ -1468,7 +1468,7 @@ class RDKitToolkitWrapper(base_wrapper.ToolkitWrapper):
 
         # Apply the greedy selection process.
         selected_indices = [0]
-        angstrom_tol = rms_tolerance.value_in_unit(unit.angstrom)
+        angstrom_tol = rms_tolerance.m_as(unit.angstrom)
         for i in range(min(limit, molecule.n_conformers) - 1):
             selected_rms = rms_matrix[selected_indices]
             any_too_close = np.any(selected_rms < angstrom_tol, axis=0)