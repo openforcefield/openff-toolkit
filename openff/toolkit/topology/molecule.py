1774dd523de2967a82d8f05693621caff47ee49c
Matt Thompson <mattwthompson@protonmail.com>
2022-10-24 19:20:13 -0500

    Sort hierarchy iterators without `packaging` (#1418)
    * Semantically sort without using `packaging.version`
    
    * Fix logic

--- openff/toolkit/topology/molecule.py
+++ openff/toolkit/topology/molecule.py
@@ -49,7 +49,6 @@ import numpy as np
 from openff.units import unit
 from openff.units.elements import MASSES, SYMBOLS
 from openff.utilities.exceptions import MissingOptionalDependencyError
-from packaging import version
 
 import openff.toolkit
 from openff.toolkit.utils.exceptions import (
@@ -5667,11 +5666,9 @@ class HierarchyScheme:
         Semantically sort the HierarchyElements belonging to this object, according to
         their identifiers.
         """
-        # hard-code the sort_func value here, since it's hard to serialize safely
-        def sort_func(x):
-            return version.parse(".".join([str(i) for i in x.identifier]))
-
-        self.hierarchy_elements.sort(key=sort_func)
+        self.hierarchy_elements.sort(
+            key=lambda x: ".".join([str(i) for i in x.identifier])
+        )
 
     def __str__(self):
         return (
