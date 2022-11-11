32f70b6f1a7d68ed268c75b04326fa0006def4c1
Jeff Wagner <jwagnerjpl@gmail.com>
2022-11-11 14:42:43 -0800

    Band-aid #1432 and #1453 (#1460)
    * add a bandaid to support loading polymers if there's a problem with stereo
    
    * update releasehistory
    
    * add test using neutral arginine from #1453
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add neutral arg pdb
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- openff/toolkit/tests/test_molecule.py
+++ openff/toolkit/tests/test_molecule.py
@@ -3796,6 +3796,21 @@ class TestMoleculeFromPDB:
             expected_mol, atom_stereochemistry_matching=False
         )
 
+    def test_molecule_from_pdb_neutral_arg_dipeptide(self):
+        offmol = Molecule.from_polymer_pdb(
+            get_data_file_path("proteins/neutral_arginine.pdb")
+        )
+        assert offmol.n_atoms == 26
+        assert offmol.total_charge == 0 * unit.elementary_charge
+        expected_mol = Molecule.from_smiles(
+            "[NH3+][C@H](CCCNC(N)(=N))C(=O)[O-]", allow_undefined_stereo=True
+        )
+        assert offmol.is_isomorphic_with(
+            expected_mol,
+            atom_stereochemistry_matching=False,
+            bond_stereochemistry_matching=False,
+        )
+
     def test_molecule_from_pdb_error_no_hydrogens(self):
         """Test that a PDB without hydrogens raises a descriptive error"""
         with pytest.raises(
