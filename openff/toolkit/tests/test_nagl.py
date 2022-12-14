9a390ed1e788eaea4c9bc0185b6a52174a93670f
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-12-14 12:53:09 -0600

    Add sanity check of partial charges

--- openff/toolkit/tests/test_nagl.py
+++ openff/toolkit/tests/test_nagl.py
@@ -1,8 +1,35 @@
+import numpy
 from openff.utilities.testing import skip_if_missing
 
+from openff.toolkit.tests.create_molecules import create_ethanol
+from openff.toolkit.tests.utils import requires_openeye
 from openff.toolkit.utils._nagl_wrapper import _NAGLToolkitWrapper
+from openff.toolkit.utils.openeye_wrapper import OpenEyeToolkitWrapper
 
 
 @skip_if_missing("openff.nagl")
-def test_version():
-    assert "0.1.0" in _NAGLToolkitWrapper()._toolkit_version
+class TestNAGLToolkitWrapper:
+    def test_version(self):
+        assert "0.1.0" in _NAGLToolkitWrapper()._toolkit_version
+
+    @requires_openeye
+    def test_charges_sanity(self):
+        molecule = create_ethanol()
+
+        molecule.assign_partial_charges(
+            partial_charge_method="am1bccelf10",
+            toolkit_registry=OpenEyeToolkitWrapper(),
+        )
+
+        openeye_charges = molecule.partial_charges
+
+        molecule = create_ethanol()
+
+        molecule.assign_partial_charges(
+            partial_charge_method="_nagl_am1bccelf10",
+            toolkit_registry=_NAGLToolkitWrapper(),
+        )
+
+        nagl_charges = molecule.partial_charges
+
+        assert numpy.allclose(openeye_charges, nagl_charges)
