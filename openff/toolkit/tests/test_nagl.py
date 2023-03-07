54658c71f676a0248051a83cc3800d09dd1350a5
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-03-07 12:06:53 -0600

    Beef up tests

--- openff/toolkit/tests/test_nagl.py
+++ openff/toolkit/tests/test_nagl.py
@@ -2,7 +2,13 @@ import numpy
 import pytest
 from openff.utilities.testing import skip_if_missing
 
-from openff.toolkit.tests.create_molecules import create_ethanol
+from openff.toolkit.tests.create_molecules import (
+    create_acetaldehyde,
+    create_cis_1_2_dichloroethene,
+    create_cyclohexane,
+    create_ethanol,
+    create_reversed_ethanol,
+)
 from openff.toolkit.tests.utils import requires_openeye
 from openff.toolkit.utils._nagl_wrapper import _NAGLToolkitWrapper
 from openff.toolkit.utils.openeye_wrapper import OpenEyeToolkitWrapper
@@ -16,8 +22,17 @@ class TestNAGLToolkitWrapper:
         assert parsed_version == _NAGLToolkitWrapper()._toolkit_version
 
     @requires_openeye
-    def test_assign_partial_charges_basic(self):
-        molecule = create_ethanol()
+    @pytest.mark.parametrize(
+        "molecule_function",
+        [
+            create_ethanol,
+            create_cis_1_2_dichloroethene,
+            create_acetaldehyde,
+            create_cyclohexane,
+        ],
+    )
+    def test_assign_partial_charges_basic(self, molecule_function):
+        molecule = molecule_function()
 
         molecule.assign_partial_charges(
             partial_charge_method="am1bccelf10",
@@ -26,7 +41,7 @@ class TestNAGLToolkitWrapper:
 
         openeye_charges = molecule.partial_charges
 
-        molecule = create_ethanol()
+        molecule = molecule_function()
 
         molecule.assign_partial_charges(
             partial_charge_method="_nagl_am1bccelf10",
@@ -36,6 +51,19 @@ class TestNAGLToolkitWrapper:
         nagl_charges = molecule.partial_charges
 
         assert numpy.allclose(openeye_charges, nagl_charges)
+
+    def test_atom_order_dependence(self):
+        """Regression test against atom order dependence."""
+        forward = create_ethanol()
+        reverse = create_reversed_ethanol()
+
+        for molecule in [forward, reverse]:
+            molecule.assign_partial_charges(
+                partial_charge_method="am1bccelf10",
+                toolkit_registry=_NAGLToolkitWrapper(),
+            )
+
+        assert numpy.allclose(forward.partial_charges, reverse.partial_charges[::-1])
 
     def test_unsupported_charge_method(self):
         with pytest.raises(ValueError, match="Charge model hartree_fock not supported"):
