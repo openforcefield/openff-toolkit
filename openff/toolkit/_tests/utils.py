5a96a7666c5b6ed739b9e39ecdfe5240ae34200b
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-08-04 10:46:22 -0500

    Drop QCPortal tests until it works with Pydantic v2

--- openff/toolkit/_tests/utils.py
+++ openff/toolkit/_tests/utils.py
@@ -20,6 +20,7 @@ import pytest
 from openff.units import unit
 from openff.units.openmm import to_openmm
 from openmm import unit as openmm_unit
+from openff.utilities import skip_if_missing, has_package
 
 from openff.toolkit.utils import (
     AmberToolsToolkitWrapper,
@@ -46,60 +47,9 @@ requires_openeye_mol2 = pytest.mark.skipif(
 )
 
 
-def has_pkg(pkg_name):
-    """
-    Helper function to generically check if a package is installed. Intended
-    to be used to check for optional dependencies.
+has_pkg = has_package
 
-    Parameters
-    ----------
-    pkg_name : str
-        The name of the package to check the availability of
-
-    Returns
-    -------
-    pkg_available : bool
-        Boolean indicator if the package is available or not
-
-    Examples
-    --------
-    >>> has_numpy = has_pkg('numpy')
-    >>> has_numpy
-    True
-    >>> has_foo = has_pkg('other_non_installed_pkg')
-    >>> has_foo
-    False
-    """
-    try:
-        importlib.import_module(pkg_name)
-    except ModuleNotFoundError:
-        return False
-    return True
-
-
-def requires_pkg(pkg_name, reason=None):
-    """
-    Helper function to generate a pytest.mark.skipif decorator
-    for any package. This allows tests to be skipped if some
-    optional dependency is not found.
-
-    Parameters
-    ----------
-    pkg_name : str
-        The name of the package that is required for a test(s)
-    reason : str, optional
-        Explanation of why the skipped it to be tested
-
-    Returns
-    -------
-    requires_pkg : _pytest.mark.structures.MarkDecorator
-        A pytest decorator that will skip tests if the package is not available
-    """
-    if not reason:
-        reason = f"Package {pkg_name} is required, but was not found."
-    requires_pkg = pytest.mark.skipif(not has_pkg(pkg_name), reason=reason)
-    return requires_pkg
-
+requires_pkg = skip_if_missing
 
 @contextmanager
 def does_not_raise():
