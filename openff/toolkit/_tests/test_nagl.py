241194e895832114d1d52e7f52d36f877ac4cc17
Lily Wang <lily@mdanalysis.org>
2023-08-30 11:51:04 +1000

    add warnings

--- openff/toolkit/_tests/test_nagl.py
+++ openff/toolkit/_tests/test_nagl.py
@@ -1,3 +1,5 @@
+import re
+
 import numpy
 import pytest
 from openff.units import unit
@@ -10,6 +12,7 @@ from openff.toolkit._tests.create_molecules import (
     create_ethanol,
     create_reversed_ethanol,
 )
+from openff.toolkit import Molecule
 from openff.toolkit._tests.utils import requires_openeye
 from openff.toolkit.utils._nagl_wrapper import _NAGLToolkitWrapper
 from openff.toolkit.utils.openeye_wrapper import OpenEyeToolkitWrapper
@@ -57,8 +60,7 @@ class TestNAGLToolkitWrapper:
         numpy.testing.assert_allclose(
             openeye_charges.m_as(unit.elementary_charge),
             nagl_charges,
-            atol=0.03,
-            rtol=0,
+            atol=0.07
         )
 
     def test_atom_order_dependence(self):
@@ -71,16 +73,33 @@ class TestNAGLToolkitWrapper:
                 partial_charge_method="_nagl_am1bccelf10",
                 toolkit_registry=_NAGLToolkitWrapper(),
             )
-
         numpy.testing.assert_allclose(
             forward.partial_charges,
             reverse.partial_charges[::-1],
-            rtol=1e-6,
+            atol=1e-7,
         )
 
     def test_unsupported_charge_method(self):
         with pytest.raises(ValueError, match="Charge model hartree_fock not supported"):
             create_ethanol().assign_partial_charges(
                 partial_charge_method="hartree_fock",
+                toolkit_registry=_NAGLToolkitWrapper(),
+            )
+
+
+    def test_unsupported_molecule_element(self):
+        si = Molecule.from_smiles("[Si+4]")
+        with pytest.raises(ValueError, match="Molecule contains forbidden element 14"):
+            si.assign_partial_charges(
+                partial_charge_method="_nagl_am1bccelf10",
+                toolkit_registry=_NAGLToolkitWrapper(),
+            )
+    
+    def test_unsupported_molecule_bond(self):
+        mol = Molecule.from_smiles("C=[Cl+1]")
+        err = re.escape("Molecule contains forbidden SMARTS pattern [#17:1]#,:,=[*:2]")
+        with pytest.raises(ValueError, match=err):
+            mol.assign_partial_charges(
+                partial_charge_method="_nagl_am1bccelf10",
                 toolkit_registry=_NAGLToolkitWrapper(),
             )
