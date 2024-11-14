1dd230f9b14428bcabf5424a4310881794c6ba5a
Jeffrey Wagner <jwagnerjpl@gmail.com>
2024-11-14 09:03:57 -0800

    update nagl failure test mol to avoid success due to new lookup table

--- openff/toolkit/_tests/test_nagl.py
+++ openff/toolkit/_tests/test_nagl.py
@@ -139,9 +139,8 @@ class TestNAGLToolkitWrapper:
                 toolkit_registry=NAGLToolkitWrapper(),
             )
 
-    @pytest.mark.xfail(reason="We need to find a new mol that fails this check in nagl 0.5")
     def test_unsupported_molecule_bond(self):
-        mol = Molecule.from_smiles("C=[Cl+1]")
+        mol = Molecule.from_smiles("CCC=[Cl+]")
         err = re.escape("Molecule contains forbidden SMARTS pattern [#17:1]#,:,=[*:2]")
         with pytest.raises(ValueError, match=err):
             mol.assign_partial_charges(
