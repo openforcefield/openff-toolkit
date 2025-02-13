37e21092663ecbc9a464160fce2b16ba6cdfdf0f
Matt Thompson <mattwthompson@protonmail.com>
2025-02-13 16:04:27 -0600

    Support Pint 0.24.4 / `openff-units 0.3.0` (#1990)
    * Use Pint 0.24.4
    
    * Shim
    
    * Small reorg
    
    * Use possible 0.3.0
    
    * Try using Pint 0.24
    
    * Debug
    
    * Debug
    
    * Be sad about typing regressions
    
    * Debug
    
    * Don't run examples
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Do not trust `Unit.compatible_units`
    
    * Do not trust `compatible_units` in topology processing
    
    * Fix
    
    * No NAGL
    
    * Remove shim
    
    * Revert "No NAGL"
    
    This reverts commit 0bb89215c7f98e0c3e2a885887fe4855ececebc7.
    
    * Revert "Don't run examples"
    
    This reverts commit 880b6536f039a529f2949d5af210fe3596f9a541.
    
    * Test both "new" and "old" upstreams
    
    * Update release history
    
    * Minor typo
    
    ---------
    
    Co-authored-by: Jeff Wagner <jeffrey.wagner+github@openforcefield.org>

--- openff/toolkit/topology/molecule.py
+++ openff/toolkit/topology/molecule.py
@@ -46,6 +46,7 @@ from typing import (
 )
 
 import numpy as np
+from openff.units import Unit
 from openff.units.elements import MASSES, SYMBOLS
 from openff.utilities.exceptions import MissingOptionalDependencyError
 from typing_extensions import TypeAlias
@@ -109,6 +110,8 @@ P = TypeVar("P", bound="Particle")
 A = TypeVar("A", bound="Atom")
 B = TypeVar("B", bound="Bond")
 
+
+_CHARGE_UNITS = set([Unit("elementary_charge")])
 
 class MoleculeDeprecationWarning(UserWarning):
     """Warning for deprecated portions of the Molecule API."""
@@ -342,12 +345,10 @@ class Atom(Particle):
         Set the atom's formal charge. Accepts either ints or unit-wrapped ints with units of charge.
         """
         if isinstance(other, int):
-            self._formal_charge = Quantity(other, unit.elementary_charge)
+            self._formal_charge = Quantity(other, "elementary_charge")
         elif isinstance(other, Quantity):
             # Faster to check equality than convert, so short-circuit
-            if other.units is unit.elementary_charge:
-                self.formal_charge = other
-            elif other.units in unit.elementary_charge.compatible_units():
+            if other.units in _CHARGE_UNITS:
                 self._formal_charge = other
             else:
                 raise IncompatibleUnitError(
@@ -365,7 +366,7 @@ class Atom(Particle):
             from openff.units.openmm import from_openmm
 
             converted = from_openmm(other)
-            if converted.units in unit.elementary_charge.compatible_units():
+            if converted.units in _CHARGE_UNITS:
                 self._formal_charge = converted
             else:
                 raise IncompatibleUnitError(
@@ -3083,14 +3084,14 @@ class FrozenMolecule(Serializable):
         index
             The index of this conformer
         """
-        if coordinates.shape != (self.n_atoms, 3):
+        if coordinates.shape != (self.n_atoms, 3):  # type: ignore[attr-defined]
             raise InvalidConformerError(
                 "molecule.add_conformer given input of the wrong shape: "
-                f"Given {coordinates.shape}, expected {(self.n_atoms, 3)}"
+                f"Given {coordinates.shape}, expected {(self.n_atoms, 3)}"  # type: ignore[attr-defined]
             )
 
         if isinstance(coordinates, Quantity):
-            if not coordinates.units.is_compatible_with(unit.angstrom):
+            if not coordinates.units.is_compatible_with(unit.angstrom):  # type: ignore[attr-defined]
                 raise IncompatibleUnitError(
                     "Coordinates passed to Molecule._add_conformer with incompatible units. "
                     "Ensure that units are dimension of length."
@@ -3125,7 +3126,7 @@ class FrozenMolecule(Serializable):
             np.zeros(shape=(self.n_atoms, 3), dtype=float), unit.angstrom
         )
         try:
-            tmp_conf[:] = coordinates
+            tmp_conf[:] = coordinates  # type: ignore[index]
         except AttributeError as e:
             # TODO: Make this a warning, log it, or do something other than print
             print(e)
@@ -3178,12 +3179,12 @@ class FrozenMolecule(Serializable):
             )
 
         if isinstance(charges, Quantity):
-            if charges.units in unit.elementary_charge.compatible_units():
+            if charges.units in _CHARGE_UNITS:
                 self._partial_charges = charges.astype(float)
             else:
                 raise IncompatibleUnitError(
                     "Unsupported unit passed to partial_charges setter. "
-                    f"Found unit {charges.units}, expected {unit.elementary_charge}"
+                    f"Found unit {charges.units}, expected elementary_charge"
                 )
 
         elif hasattr(charges, "unit"):
@@ -3199,12 +3200,12 @@ class FrozenMolecule(Serializable):
                 from openff.units.openmm import from_openmm
 
                 converted = from_openmm(charges)
-                if converted.units in unit.elementary_charge.compatible_units():
+                if converted.units in _CHARGE_UNITS:
                     self._partial_charges = converted.astype(float)
                 else:
                     raise IncompatibleUnitError(
                         "Unsupported unit passed to partial_charges setter. "
-                        f"Found unit {converted.units}, expected {unit.elementary_charge}"
+                        f"Found unit {converted.units}, expected elementary_charge"
                     )
 
         else:
@@ -4118,7 +4119,7 @@ class FrozenMolecule(Serializable):
         # add the data to the xyz_data list
         for i, geometry in enumerate(conformers, 1):
             xyz_data.write(f"{self.n_atoms}\n" + title(end))
-            for j, atom_coords in enumerate(geometry.m_as(unit.angstrom)):
+            for j, atom_coords in enumerate(geometry.m_as(unit.angstrom)):  # type: ignore[arg-type]
                 x, y, z = atom_coords
                 xyz_data.write(
                     f"{SYMBOLS[self.atoms[j].atomic_number]}       {x: .10f}   {y: .10f}   {z: .10f}\n"
