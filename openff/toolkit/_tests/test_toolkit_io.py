d35922c581777e388ff5f6607da28c5212933cbb
Jeff Wagner <jwagnerjpl@gmail.com>
2025-07-07 13:16:12 -0700

    suggested change from ruff

--- openff/toolkit/_tests/test_toolkit_io.py
+++ openff/toolkit/_tests/test_toolkit_io.py
@@ -1121,4 +1121,4 @@ class TestRDKitToolkitSmiles(BaseSmiles):
 
 
 if __name__ == "__main__":
-    sys.exit(pytest.main([__file__] + sys.argv[1:]))
+    sys.exit(pytest.main([__file__, *sys.argv[1:]]))
